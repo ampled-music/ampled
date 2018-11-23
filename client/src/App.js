f595da2a7bc27e3f81fe6584de70e6bb5a9eeaee
bentona <benton.anderson@gmail.com>
2018-11-23 12:51:00 -0600

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,9 @@ class App extends Component {
     };
   }
   componentWillMount(){
-    $.ajax({
-      method: "GET",
-      url: "auth/is_signed_in.json"
-    }).done((data) => {
-      this.setState({signedIn: data.signed_in});
-    })
+
   }
 
-  getInitialState()
   render() {
     return (
       <div className="App">

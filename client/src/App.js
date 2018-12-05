87f9b336c3260a2393af92adbfc5e30fc07f3f59
Deshon, Ryan <ryandeshon@gmail.com>
2018-12-04 19:11:53 -0500

    Init of early frontend work

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.scss";
-import Counter from "./components/Counter";
+import "./App.css";
+
+import Counter      from "./components/Counter";
+import Nav          from "./components/Nav";
+import ArtistHeader from "./components/Artist_Header";
+import ArtistInfo   from "./components/Artist_Info";
+
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <header className="App__header">
-          <Counter />
-          <img src={logo} className="App__logo" alt="logo" />
-          <p>
-            Edit <code className="App__code">src/App.js</code> and save to
-            reload.
-          </p>
-          <a
-            className="App__link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Nav />
+          <ArtistHeader />
+          <ArtistInfo />
       </div>
     );
   }
---
+++ client/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from './ampled_logo.svg';
+
+export default class Nav extends React.Component {
+  render() {
+    return (
+        <header className="header">
+            <img src={logo} alt="logo" className="logo" />
+        </header>
+    );
+  }
+}

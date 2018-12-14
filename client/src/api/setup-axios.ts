b633b3dc4cfdb7240065ab1d693c43801a939a4c
bentona <benton.anderson@gmail.com>
2018-12-14 12:15:52 -0500

    add types, fix uploader

--- client/src/api/setup-axios.ts
+++ client/src/api/setup-axios.ts
@@ -11,12 +11,11 @@ export const setupAxios = () => {
   axios.defaults.baseURL = config.apiUrl;
 
   axios.interceptors.request.use((axiosConfig) => {
-    const axiosUrl = config.apiUrl === '/api' ? axiosConfig.url : `${axios.defaults.baseURL}${axiosConfig.url}`;
-    axiosConfig.url = axiosUrl;
+    // const axiosUrl = config.apiUrl === '/api' ? axiosConfig.url : `${axios.defaults.baseURL}${axiosConfig.url}`;
+    // axiosConfig.url = axiosUrl;
     axiosConfig.timeout = 200000;
-    axiosConfig.headers.authorization = store.get(config.localStorageKeys.token);
+    // axiosConfig.headers.authorization = store.get(config.localStorageKeys.token);
     axiosConfig.paramsSerializer = paramsSerializer;
-
     return axiosConfig;
   });
 

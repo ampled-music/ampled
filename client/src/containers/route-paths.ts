7e5011c425e26cb3c9d83476f154c2942fe8961d
Vanessa da Conceição <vanessa.sc22@gmail.com>
2018-12-17 19:06:46 -0200

    add tests for artists pages

--- client/src/containers/route-paths.ts
+++ client/src/containers/route-paths.ts
@@ -2,10 +2,11 @@ export const routePaths = {
   artists: '/artists/:id',
   createPost: '/create_post/:id',
   connect: '/connect',
+  home: '/home',
   root: '/',
   login: '/login',
   upload: '/upload',
   loggedUser: {
     root: '/logged-user',
   },
-}
+};

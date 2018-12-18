d9f3b521a53212e62cd9d996538de6bc045ea769
Vanessa da Conceição <vanessa.sc22@gmail.com>
2018-12-18 17:33:22 -0200

    get email and password and save toke in local storage

--- client/src/containers/AuthenticationGateway.tsx
+++ client/src/containers/AuthenticationGateway.tsx
@@ -4,6 +4,8 @@ import * as store from 'store';
 
 import { config } from '../config';
 import { Artist } from './artist/Artist';
+import { Connect } from './connect/Connect';
+import { Login } from './connect/Login';
 import { Home } from './home/Home';
 import { PostForm } from './posts/PostForm';
 import { Upload } from './posts/Upload';
@@ -21,6 +23,8 @@ const AuthenticationGateway = ({ component: Component, path }: { component: any;
   return (
     <Switch>
       <Route exact path={routePaths.root} component={Home} />
+      <Route exact path={routePaths.login} component={Login} />
+      <Route exact path={routePaths.connect} component={Connect} />
       <Route exact path={routePaths.artists} component={Artist} />
       <Route exact path={routePaths.upload} component={Upload} />
       <Route exact path={routePaths.createPost} component={PostForm} />
--- client/src/containers/connect/Login.tsx
+++ client/src/containers/connect/Login.tsx
@@ -1,22 +1,61 @@
-import * as React from 'react'
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
+import { config } from 'src/config';
+import { authenticate } from 'src/redux/ducks/authenticate';
+import * as store from 'store';
+import { routePaths } from '../route-paths';
 
-import { config } from '../../config'
+class LoginComponent extends React.Component<any, any> {
+  state = {
+    username: '',
+    password: '',
+    submitted: false,
+  };
 
-import './login.scss'
+  componentDidMount() {
+    const token = store.get(config.localStorageKeys.token);
+    if (token) {
+      this.props.dispatch(authenticate(token));
+    }
+  }
 
-const Login = () => (
-  <div className="login">
-    <p>Sign in with:</p>
-    <a href={config.authUrl.google}>
-      <button>Google</button>
-    </a>
-    <a href={config.authUrl.facebook}>
-      <button>Facebook</button>
-    </a>
-    <a href={config.authUrl.linkedIn}>
-      <button>LinkedIn</button>
-    </a>
-  </div>
-)
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ submitted: true });
+  };
 
-export { Login }
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    if (this.state.submitted) {
+      return <Redirect to={routePaths.connect} />;
+    }
+
+    if (this.props.authentication.authenticated) {
+      return <Redirect to={routePaths.root} />;
+    }
+
+    return (
+      <div className="login">
+        <h1>Login</h1>
+        <form name="login" onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="email" name="username" onChange={() => this.handleChange} required />
+          <input type="password" placeholder="password" name="password" onChange={() => this.handleChange} required />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state: any) => ({
+  authentication: state.authentication,
+});
+
+const Login = connect(mapStateToProps)(LoginComponent);
+
+export { Login };

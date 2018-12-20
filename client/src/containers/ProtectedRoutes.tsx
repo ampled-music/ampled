b26e2b8eecad731cc4d48ee8679a94a11d1712e0
Vanessa da Conceição <vanessa.sc22@gmail.com>
2018-12-20 19:07:53 -0200

    redirect user to / if the route doesn't exist

--- client/src/containers/ProtectedRoutes.tsx
+++ client/src/containers/ProtectedRoutes.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import { Artist } from './artist/Artist';
 import { routePaths } from './route-paths';
@@ -12,6 +12,7 @@ export class ProtectedRoutesComponent extends React.Component<any, any> {
       <Switch>
         <Route exact path={routePaths.loggedUser.root} component={User} />
         <Route exact path={routePaths.artists} component={Artist} />
+        <Route render={() => <Redirect to={routePaths.root} />} />
       </Switch>
     );
   }

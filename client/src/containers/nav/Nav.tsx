9fbc6b2eda6094ab3846663981b6d6e766780d97
Vanessa da Conceição <vanessa.sc22@gmail.com>
2018-12-21 12:51:03 -0200

    add menu icon to nav bar

--- client/src/containers/nav/Nav.tsx
+++ client/src/containers/nav/Nav.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import logo from '../../images/ampled_logo.svg';
+import menu from '../../images/menu.svg';
 
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <header className="header">
-      <Link to="/">
-        <img src={logo} alt="logo" className="logo" height="100%" />
+      <Link className="logo" to="/">
+        <img src={logo} alt="logo" height="100%" />
+      </Link>
+      <Link to="/login">
+        <img src={menu} alt="logo" className="menu" />
       </Link>
     </header>
   );

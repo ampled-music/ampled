da4955d6e307650fd742c043ed1a667a195eb8c8
bentona <benton.anderson@gmail.com>
2018-12-28 13:18:33 -0500

    working jwt login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
   include Pundit
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session, unless: -> { request.format.json? }
   before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,17 @@ module AmpledWeb
       g.request_specs    false
     end
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+
+        resource '*',
+                 headers: :any,
+                 methods: [:get, :post, :put, :patch, :delete, :options, :head],
+                 expose: ['Authorization']
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

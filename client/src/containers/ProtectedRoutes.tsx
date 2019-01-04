441f7f77fcbf457b149646823cfbd34e9bae3611
Vanessa da Conceição <vanessa.sc22@gmail.com>
2019-01-04 16:40:16 -0200

    remove artist page from protected routes so Austin can see it

--- client/src/containers/ProtectedRoutes.tsx
+++ client/src/containers/ProtectedRoutes.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
-import { Artist } from './artist/Artist';
 import { routePaths } from './route-paths';
 import { User } from './user/User';
 
@@ -11,7 +10,6 @@ export class ProtectedRoutesComponent extends React.Component {
     return (
       <Switch>
         <Route exact path={routePaths.loggedUser.root} component={User} />
-        <Route exact path={routePaths.artists} component={Artist} />
         <Route render={() => <Redirect to={routePaths.root} />} />
       </Switch>
     );

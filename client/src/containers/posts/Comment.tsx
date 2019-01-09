9f23c2c8462803be7d1ff4b35ad1efc642949a2c
Vanessa da Conceição <vanessa.sc22@gmail.com>
2019-01-09 11:57:07 -0200

    show comment time

--- client/src/containers/posts/Comment.tsx
+++ client/src/containers/posts/Comment.tsx
@@ -6,7 +6,7 @@ const Comment = (commentData) => {
   return (
     <div key={`comment-${comment.id}`} id={`comment-${comment.id}`}>
       <b>{comment.author}</b>
-      <span style={{ float: 'right' }}>{comment.created_at} ago</span>
+      <span style={{ float: 'right' }}>{comment.created_ago} ago</span>
       <p>{comment.text}</p>
     </div>
   );

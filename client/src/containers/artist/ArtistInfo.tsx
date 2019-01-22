34050aab0cbc1896ca5902b4b9c259c76371e4d6
Deshon, Ryan <ryandeshon@gmail.com>
2019-01-22 18:48:40 -0500

    Added instagram and twitter handles

--- client/src/containers/artist/ArtistInfo.tsx
+++ client/src/containers/artist/ArtistInfo.tsx
@@ -2,30 +2,36 @@ import * as React from 'react';
 
 interface Props {
   location: string;
+  twitterHandle: string;
+  instagramHandle: string;
 }
 
 class ArtistInfo extends React.Component<Props, any> {
+  
   render() {
+
+    const { location, twitterHandle, instagramHandle } = this.props;
+
     return (
       <div className="artist-info container">
         <div className="row justify-content-between">
           <div className="col-md-4">
             <div className="artist-info__location">
-              <i className="fas fa-map-marker-alt" /> {this.props.location}
+              <i className="fas fa-map-marker-alt" /> {location}
             </div>
           </div>
           <div className="col-md-5">
             <div className="artist-info__social">
               <div className="artist-info__social_twitter">
                 <i className="fab fa-twitter" />{' '}
-                <a href="https://twitter.com/nineinchnails" target="_blank">
-                  @nin
+                <a href="https://twitter.com/{twitterHandle}" target="_blank">
+                  @{twitterHandle}
                 </a>
               </div>
               <div className="artist-info__social_instagram">
                 <i className="fab fa-instagram" />{' '}
-                <a href="https://instagram.com/nineinchnails" target="_blank">
-                  #nineinchnails
+                <a href="https://instagram.com/{instagramHandle}" target="_blank">
+                  #{instagramHandle}
                 </a>
               </div>
             </div>

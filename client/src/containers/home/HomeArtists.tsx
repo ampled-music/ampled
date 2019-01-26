facc8a0586d3ad150ad7edb7cc6ed7addf89f823
Deshon, Ryan <ryandeshon@gmail.com>
2019-01-26 14:26:49 -0500

    Styling for Top 3 sections of home page

--- client/src/containers/home/HomeArtists.tsx
+++ client/src/containers/home/HomeArtists.tsx
@@ -6,73 +6,75 @@ import { bindActionCreators } from 'redux';
 import { artistsPages } from '../../redux/ducks/get-artists-pages';
 
 interface Props {
-  getArtistsPages: Function;
-  artistsPages: {
-    loading: boolean;
-    pages: [];
-  };
+    getArtistsPages: Function;
+    artistsPages: {
+        loading: boolean;
+        pages: [];
+    };
 }
 
 interface State {
-  artistPages: [];
+    artistPages: [];
 }
 
 class HomeArtistsComponent extends React.Component<Props, State> {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      artistPages: [],
-    };
-  }
-
-  componentDidMount() {
-    this.props.getArtistsPages();
-  }
-  
-  render() {
-
-    const loading = this.props.artistsPages.loading;
-    const artistsPages = this.props.artistsPages.pages;
-
-    if (loading) {
-      return <span>Loading...</span>;
+    constructor(props) {
+        super(props);
+        this.state = {
+            artistPages: [],
+        };
     }
 
-    return (
-      <div className="home-artists container">
-        {this.getArtistsList(artistsPages)}
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.props.getArtistsPages();
+    }
 
-  private getArtistsList(artistsPages: any) {
-    return artistsPages.map((page) => {
-      return (
-        <div key={page.id}>
-          <Link to={`/artists/${page.id}`}>{page.name}</Link>
-        </div>
-      );
-    });
-  }
+    render() {
+
+        const loading = this.props.artistsPages.loading;
+        const artistsPages = this.props.artistsPages.pages;
+
+        if (loading) {
+            return <span>Loading...</span>;
+        }
+
+        return (
+            <div className="home-artists">
+                <div className="container">
+                    {this.getArtistsList(artistsPages)}
+                </div>
+            </div>
+        );
+    }
+
+    private getArtistsList(artistsPages: any) {
+        return artistsPages.map((page) => {
+            return (
+                <div key={page.id}>
+                    <Link to={`/artists/${page.id}`}>{page.name}</Link>
+                </div>
+            );
+        });
+    }
 
 }
 
 const mapStateToProps = (state) => {
-  return {
-    artistsPages: state.pages,
-  };
+    return {
+        artistsPages: state.pages,
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    getArtistsPages: bindActionCreators(artistsPages, dispatch),
-  };
+    return {
+        getArtistsPages: bindActionCreators(artistsPages, dispatch),
+    };
 };
 
 const HomeArtists = connect(
-  mapStateToProps,
-  mapDispatchToProps,
+    mapStateToProps,
+    mapDispatchToProps,
 )(HomeArtistsComponent);
 
 export { HomeArtists };
--- client/src/containers/home/HomeHeader.tsx
+++ client/src/containers/home/HomeHeader.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
 
+import header_collage from '../../images/home/home_header_collage.png';
+import support from '../../images/home/home_header_support.png';
+
+
 interface Props {
     //   location: string;
 }
@@ -7,15 +11,22 @@ interface Props {
 class HomeHeader extends React.Component<Props, any> {
     render() {
         return (
-            <div className="home-header container">
-                <div className="row">
-                    <div className="col-md-4">
-                        <div className="home-header__info">
-                            Ampled is a space where music artists post unreleased, unique, or exclusive content and are directly supported on a monthly basis by their audience.
+            <div className="home-header">
+                <div className="container">
+                    <img className="home-header__support" src={ support } />
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="home-header__info">
+                                Ampled is a space where music artists post unreleased, unique, or exclusive content and are directly supported on a monthly basis by their audience.
+                            </div>
+                            <button className="home-header__button btn">
+                                Create an Artist Page
+                            </button>
                         </div>
-                        <button className="home-header__button btn">
-                            Create an Artist Page
-                        </button>
+
+                        <div className="col-md-8">
+                            <img src={header_collage} className="home-header__collage" />
+                        </div>
                     </div>
                 </div>
             </div>

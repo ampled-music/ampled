774c1eece3654930c492c04b861a5c6ce7d75717
Deshon, Ryan <ryandeshon@gmail.com>
2019-01-26 15:26:47 -0500

    Two to Four spaces

--- client/src/containers/artist/ArtistHeader.tsx
+++ client/src/containers/artist/ArtistHeader.tsx
@@ -9,94 +9,94 @@ import trent_reznor from '../../test/trent_reznor.jpg';
 import './artist.scss';
 
 interface Props {
-  name: string;
-  id: number;
-  accentColor: string;
-  bannerImageUrl: string;
-  openPostModal: React.MouseEventHandler;
-  userAuthenticated: boolean;
+    name: string;
+    id: number;
+    accentColor: string;
+    bannerImageUrl: string;
+    openPostModal: React.MouseEventHandler;
+    userAuthenticated: boolean;
 }
 
 class ArtistHeader extends React.Component<Props, any> {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      showConfirmationDialog: false,
-    };
-  }
+        this.state = {
+            showConfirmationDialog: false,
+        };
+    }
 
-  render() {
-    const { name, accentColor, bannerImageUrl, userAuthenticated } = this.props;
+    render() {
+        const { name, accentColor, bannerImageUrl, userAuthenticated } = this.props;
 
-    return (
-      <div className="artist-header container">
-        {' '}
-        {/* Main Section */}
-        <div className="row">
-          <div className="col-md-8">
-            {/* Left Side */}
-            <div className="artist-header__title">{name}</div>
+        return (
+            <div className="artist-header container">
+                {' '}
+                {/* Main Section */}
+                <div className="row">
+                    <div className="col-md-8">
+                        {/* Left Side */}
+                        <div className="artist-header__title">{name}</div>
 
-            <div className="row">
-              <div className="col-3 artist-header__members" style={{ borderColor: accentColor }}>
-                <img className="artist-header__member" style={{ borderColor: accentColor }} src={trent_reznor} />
-                <img className="artist-header__member" style={{ borderColor: accentColor }} src={atticus_ross} />
-              </div>
-              <div className="col-9 artist-header__photos" style={{ borderColor: accentColor }}>
-                <img className="artist-header__photo" src={bannerImageUrl} />
-              </div>
-            </div>
-          </div>
-          {/* Close Left Side */}
+                        <div className="row">
+                            <div className="col-3 artist-header__members" style={{ borderColor: accentColor }}>
+                                <img className="artist-header__member" style={{ borderColor: accentColor }} src={trent_reznor} />
+                                <img className="artist-header__member" style={{ borderColor: accentColor }} src={atticus_ross} />
+                            </div>
+                            <div className="col-9 artist-header__photos" style={{ borderColor: accentColor }}>
+                                <img className="artist-header__photo" src={bannerImageUrl} />
+                            </div>
+                        </div>
+                    </div>
+                    {/* Close Left Side */}
 
-          <div className="col-md-4">
-            {/* Right Side */}
-            <div className="artist-header__message">A Message From The Band</div>
-            {userAuthenticated && (
-              <div className="new-post">
-                <button onClick={this.props.openPostModal}>
-                  <span>New Post</span>
-                  <FontAwesomeIcon icon={faPlus} color="#ffffff" />
-                </button>
-              </div>
-            )}
+                    <div className="col-md-4">
+                        {/* Right Side */}
+                        <div className="artist-header__message">A Message From The Band</div>
+                        {userAuthenticated && (
+                            <div className="new-post">
+                                <button onClick={this.props.openPostModal}>
+                                    <span>New Post</span>
+                                    <FontAwesomeIcon icon={faPlus} color="#ffffff" />
+                                </button>
+                            </div>
+                        )}
 
-            <div className="artist-header__message-container" style={{ borderColor: accentColor }}>
-              <img className="artist-header__message-image" src={nin_video} />
-            </div>
+                        <div className="artist-header__message-container" style={{ borderColor: accentColor }}>
+                            <img className="artist-header__message-image" src={nin_video} />
+                        </div>
 
-            <div className="artist-header__supporters">
-              <div className="artist-header__supporter-title">2 Supporters</div>
+                        <div className="artist-header__supporters">
+                            <div className="artist-header__supporter-title">2 Supporters</div>
 
-              <div className="row align-items-center">
-                <div className="col-3">
-                  <img className="artist-header__member" style={{ borderColor: accentColor }} src={trent_reznor} />
-                </div>
-                <div className="col-9">
-                  <div className="artist-header__member_name">Trent R.</div>
-                  <div className="artist-header__member_quote">
-                    I think there's something strangely musical about noise.
+                            <div className="row align-items-center">
+                                <div className="col-3">
+                                    <img className="artist-header__member" style={{ borderColor: accentColor }} src={trent_reznor} />
+                                </div>
+                                <div className="col-9">
+                                    <div className="artist-header__member_name">Trent R.</div>
+                                    <div className="artist-header__member_quote">
+                                        I think there's something strangely musical about noise.
                   </div>
-                </div>
-              </div>
+                                </div>
+                            </div>
 
-              <div className="row align-items-center">
-                <div className="col-3">
-                  <img className="artist-header__member" style={{ borderColor: accentColor }} src={atticus_ross} />
+                            <div className="row align-items-center">
+                                <div className="col-3">
+                                    <img className="artist-header__member" style={{ borderColor: accentColor }} src={atticus_ross} />
+                                </div>
+                                <div className="col-9">
+                                    <div className="artist-header__member_name">Atticus R.</div>
+                                    <div className="artist-header__member_quote">Sure Trent, whatever.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Close Right Side */}
                 </div>
-                <div className="col-9">
-                  <div className="artist-header__member_name">Atticus R.</div>
-                  <div className="artist-header__member_quote">Sure Trent, whatever.</div>
-                </div>
-              </div>
             </div>
-          </div>
-          {/* Close Right Side */}
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export { ArtistHeader };

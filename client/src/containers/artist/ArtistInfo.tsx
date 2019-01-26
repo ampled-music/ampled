758d5ff665b6a5816069b0b917b9b1c53c3bae12
Deshon, Ryan <ryandeshon@gmail.com>
2019-01-26 17:17:10 -0500

    Updated Artist info Icons and color

--- client/src/containers/artist/ArtistInfo.tsx
+++ client/src/containers/artist/ArtistInfo.tsx
@@ -1,36 +1,43 @@
 import * as React from 'react';
 
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 interface Props {
   location: string;
   twitterHandle: string;
   instagramHandle: string;
+  accentColor: string;
 }
 
 class ArtistInfo extends React.Component<Props, any> {
   
   render() {
 
-    const { location, twitterHandle, instagramHandle } = this.props;
+    const { location, accentColor, twitterHandle, instagramHandle } = this.props;
 
     return (
       <div className="artist-info container">
         <div className="row justify-content-between">
           <div className="col-md-4">
             <div className="artist-info__location">
-              <i className="fas fa-map-marker-alt" /> {location}
+              <FontAwesomeIcon className="icon" icon={faMapMarkerAlt} />
+              {location}
             </div>
           </div>
           <div className="col-md-5">
             <div className="artist-info__social">
               <div className="artist-info__social_twitter">
-                <i className="fab fa-twitter" />{' '}
-                <a href="https://twitter.com/{twitterHandle}" target="_blank">
+                <FontAwesomeIcon className="icon" icon={faTwitter} />
+                <a href="https://twitter.com/{twitterHandle}" target="_blank" style={{ color: accentColor }}>
                   @{twitterHandle}
                 </a>
               </div>
               <div className="artist-info__social_instagram">
-                <i className="fab fa-instagram" />{' '}
-                <a href="https://instagram.com/{instagramHandle}" target="_blank">
+                <FontAwesomeIcon className="icon" icon={faInstagram} />
+                <a href="https://instagram.com/{instagramHandle}" target="_blank" style={{ color: accentColor }}>
                   #{instagramHandle}
                 </a>
               </div>

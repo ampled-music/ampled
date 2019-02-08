205cace53f356c65c648c5572c8360d6aa452c9c
Benton <benton.anderson@gmail.com>
2019-02-08 13:59:00 -0500

    add video screenshot url

--- app/models/artist_page.rb
+++ app/models/artist_page.rb
@@ -2,17 +2,18 @@
 #
 # Table name: artist_pages
 #
-#  accent_color     :string
-#  banner_image_url :string
-#  bio              :string
-#  created_at       :datetime         not null
-#  id               :bigint(8)        not null, primary key
-#  instagram_handle :string
-#  location         :string
-#  name             :string
-#  twitter_handle   :string
-#  updated_at       :datetime         not null
-#  video_url        :string
+#  accent_color         :string
+#  banner_image_url     :string
+#  bio                  :string
+#  created_at           :datetime         not null
+#  id                   :bigint(8)        not null, primary key
+#  instagram_handle     :string
+#  location             :string
+#  name                 :string
+#  twitter_handle       :string
+#  updated_at           :datetime         not null
+#  video_screenshot_url :string
+#  video_url            :string
 #
 
 class ArtistPage < ApplicationRecord

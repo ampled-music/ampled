82d02f3a277bd52133dc5d46abbcd1646055f7cb
Benton <benton.anderson@gmail.com>
2019-02-08 13:59:00 -0500

    add video screenshot url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_30_031834) do
+ActiveRecord::Schema.define(version: 2019_02_08_185542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_01_30_031834) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "video_url"
+    t.string "video_screenshot_url"
   end
 
   create_table "comments", force: :cascade do |t|

6501b7039d1755ffb1c33f3f8e6a23244d3cfb8e
Deshon, Ryan <ryandeshon@gmail.com>
2019-02-18 15:45:13 -0500

    Changed footer links order

--- client/src/containers/footer/Footer.tsx
+++ client/src/containers/footer/Footer.tsx
@@ -31,15 +31,15 @@ class Footer extends React.Component<Props, State> {
             </div>
             <div className="col-md-2">
               <div className="title">Get Started</div>
-              <a href="#">Create An Artist Page</a>
-              <a href="https://www.ampled.com/about">About Us</a>
               <a href="https://www.ampled.com/what-is-ampled">What is Ampled?</a>
+              <a href="#">Create An Artist Page</a>
               <a href="https://www.ampled.com/our-mission">Our Mission</a>
               <a href="https://www.ampled.com/who-we-are">Who We Are</a>
-              <a href="https://www.ampled.com/artist-ownership">Artist Ownership</a>
+              <a href="https://www.ampled.com/about">Learn More About Us</a>
             </div>
             <div className="col-md-2">
               <div className="title">Get Involved</div>
+              <a href="https://www.ampled.com/artist-ownership">Artist Ownership</a>
               <a href="https://www.ampled.com/jobs">Jobs</a>
               <a href="https://www.ampled.com/press">Press</a>
             </div>
@@ -47,9 +47,9 @@ class Footer extends React.Component<Props, State> {
               <div className="title">Get Informed</div>
               <a href="https://www.ampled.com/artist-ownership">Transparency Dash</a>
               <a href="https://www.ampled.com/faq">FAQs</a>
-              <a href="https://www.ampled.com/zine">Blog</a>
               <a href="https://www.ampled.com/terms-of-use">Terms of Use</a>
               <a href="https://www.ampled.com/privacy-policy">Privacy Policy</a>
+              <a href="https://www.ampled.com/zine">Blog</a>
               <a href="https://www.ampled.com/contact-us">Contact</a>
             </div>
             <div className="col-md-2">

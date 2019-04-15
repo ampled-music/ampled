cb0922e999a7007cbc09c4d98b424b58c15b3af8
william <grisa.william@gmail.com>
2019-04-16 11:23:14 +1200

    Move get me to app container

--- client/src/containers/app/App.tsx
+++ client/src/containers/app/App.tsx
@@ -1,13 +1,50 @@
 import '../../styles/App.css';
 
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Store } from 'src/redux/configure-store';
+import { getMeAction } from 'src/redux/me/get-me';
 
+import { initialState as loginInitialState } from '../../redux/authentication/initial-state';
+import { initialState as meInitialState } from '../../redux/me/initial-state';
 import { Routes } from '../Routes';
 
-const App = () => (
-  <div className="page">
-    <Routes />
-  </div>
-);
+type Dispatchers = ReturnType<typeof mapDispatchToProps>;
+
+type Props = typeof loginInitialState & typeof meInitialState & Dispatchers;
+
+class AppComponent extends React.Component<Props, any> {
+  componentDidMount() {
+    this.props.getMe();
+  }
+
+  componentDidUpdate() {
+    if (this.props.token && !this.props.error && !this.props.userData) {
+      this.props.getMe();
+    }
+  }
+
+  render() {
+    return (
+      <div className="page">
+        <Routes />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state: Store) => ({
+  ...state.me,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getMe: bindActionCreators(getMeAction, dispatch),
+});
+
+const App = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AppComponent);
 
 export { App };

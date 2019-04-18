1854e5ec3e1fa56c5780a12ca9b98f0de0bb97fe
Benton <benton.anderson@gmail.com>
2019-04-17 22:36:50 -0400

    add jwt revocation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_220257) do
+
+ActiveRecord::Schema.define(version: 2019_04_18_022045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_04_17_220257) do
     t.integer "artist_page_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["artist_page_id", "user_id"], name: "index_page_ownerships_on_artist_page_id_and_user_id"
     t.index ["user_id", "artist_page_id"], name: "index_page_ownerships_on_user_id_and_artist_page_id"
   end
@@ -92,9 +94,10 @@ ActiveRecord::Schema.define(version: 2019_04_17_220257) do
     t.datetime "confirmation_sent_at"
     t.datetime "locked_at"
     t.string "profile_image_url"
-    t.integer "age"
+    t.string "jti", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 

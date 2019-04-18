9457058f4cc031a9fe7d8ad31c3a0abb0677a713
Benton <benton.anderson@gmail.com>
2019-04-17 23:38:43 -0400

    test

--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -1,6 +1,10 @@
 class MeController < ApplicationController
   def index
-    @owned = current_user&.owned_pages&.map { |page| OpenStruct.new(id: page.id, role: "owner") }
-    @supported = current_user&.supported_artists&.map { |page| OpenStruct.new(id: page.id, role: "supporter") }
+    if current_user.blank?
+      render json: {}
+      return
+    end
+    @owned = current_user.owned_pages.map { |page| OpenStruct.new(id: page.id, role: "owner") }
+    @supported = current_user.supported_artists.map { |page| OpenStruct.new(id: page.id, role: "supporter") }
   end
 end

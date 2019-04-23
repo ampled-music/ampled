62ace22d9ba157040fca096c7952423f52cd9ef8
Benton <benton.anderson@gmail.com>
2019-04-23 19:37:41 -0400

    linting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,8 @@ class PagesController < ApplicationController
   def stripe_signup_url(artist_page)
     base = "https://connect.stripe.com/express/oauth/authorize"
     params = {
-      redirect_uri: 'http://localhost:3000/stripe_oauth_callback',
-      client_id: 'ca_Eowu0ycKNxFo46f8hqlCNCpt4w26bxer',
+      redirect_uri: "http://localhost:3000/stripe_oauth_callback",
+      client_id: "ca_Eowu0ycKNxFo46f8hqlCNCpt4w26bxer",
       state: artist_page.stripe_state_token,
       "suggested_capabilities[]" => "card_payments"
     }.to_query

f924b05e1d4e9d7982954474b214dbe7b2b0ad73
william <grisa.william@gmail.com>
2019-04-26 10:33:34 +1200

    Add info and cancel button

--- client/src/containers/settings/UserSettings.tsx
+++ client/src/containers/settings/UserSettings.tsx
@@ -7,7 +7,7 @@ import { bindActionCreators } from 'redux';
 import { Store } from 'src/redux/configure-store';
 import { getMeAction } from 'src/redux/me/get-me';
 
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faHeartBroken, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { closeAuthModalAction, openAuthModalAction } from 'src/redux/authentication/authentication-modal';
 import { initialState as loginInitialState } from '../../redux/authentication/initial-state';
@@ -19,6 +19,11 @@ type Dispatchers = ReturnType<typeof mapDispatchToProps>;
 type Props = typeof loginInitialState & typeof meInitialState & Dispatchers & { history: any };
 
 class UserSettingsComponent extends React.Component<Props, any> {
+  state = {
+    showCancelModal: false,
+    subscription: undefined,
+  };
+
   componentDidMount() {
     this.props.getMe();
   }
@@ -32,6 +37,14 @@ class UserSettingsComponent extends React.Component<Props, any> {
   getFormattedDate = (date: string) => {
     return DateTime.fromString(date.split('T')[0], 'yyyy-MM-dd').toLocaleString(DateTime.DATE_MED);
   };
+
+  openCancelModal = (event, subscription) => {
+    event.preventDefault();
+
+    this.setState({ showCancelModal: true, subscription });
+  };
+
+  closeCancelModal = () => this.setState({ showCancelModal: false, subscription: undefined });
 
   renderUserImage = () => {
     const { userData } = this.props;
@@ -59,24 +72,58 @@ class UserSettingsComponent extends React.Component<Props, any> {
 
   renderPagesTitle = (title: string) => <h1>{title}</h1>;
 
+  renderCancelSubscriptionModal = () => {
+    if (!this.state.subscription) {
+      return null;
+    }
+
+    return (
+      <Modal open={this.state.showCancelModal} onClose={this.closeCancelModal}>
+        <div className="user-settings-cancel-modal">
+          <p>Are you sure you want to stop supporting {this.state.subscription.name}?</p>
+          <div className="actions">
+            <button className="btn">yes</button>
+            <button className="btn" onClick={this.closeCancelModal}>
+              OF COURSE NOT!
+            </button>
+          </div>
+        </div>
+      </Modal>
+    );
+  };
+
   renderSupportedArtists = () => (
     <div className="pages">
       {this.props.userData.subscriptions.map((subscription) => (
-        <div className="artist">
+        <div key={`artist-${subscription.name}`} className="artist">
           <div className="image-border">
             <img src={subscription.image} />
           </div>
           <div className="artist-info">
             <p className="artist-name">{subscription.name}</p>
             <div className="extra-info">
-              <label>
-                SUPPORTING AT
-                <p>${subscription.amount / 100}/Month</p>
-              </label>
-              <label>
-                LAST POST
-                <p>{this.getFormattedDate(subscription.last_post_date)}</p>
-              </label>
+              <div className="support-info">
+                <div className="supporting-at">
+                  <label>
+                    <p className="info-title">SUPPORTING AT</p>
+                    <p className="supporting-at-value">${subscription.amount / 100}/Month</p>
+                  </label>
+                  <button onClick={(event) => this.openCancelModal(event, subscription)}>
+                    <FontAwesomeIcon icon={faHeartBroken} />
+                    CANCEL
+                  </button>
+                </div>
+                <div>
+                  <label>
+                    <p className="info-title">LAST POST</p>
+                    <p className="info-value">{this.getFormattedDate(subscription.last_post_date)}</p>
+                  </label>
+                  <label>
+                    <p className="info-title">SUPPORT DATE</p>
+                    <p className="info-value">{this.getFormattedDate(subscription.support_date)}</p>
+                  </label>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -105,6 +152,7 @@ class UserSettingsComponent extends React.Component<Props, any> {
           </div>
         </Modal>
         {userData && this.renderContent()}
+        {this.renderCancelSubscriptionModal()}
       </div>
     );
   }

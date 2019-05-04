7e40c7d3273e11ef1b85ee965f726b638545d5d1
Deshon, Ryan <ryandeshon@gmail.com>
2019-05-04 13:50:29 -0400

    Home Page font changes

--- client/src/containers/home/HomeHeader.tsx
+++ client/src/containers/home/HomeHeader.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
 import { config } from 'src/config';
-import support from '../../images/home/home_header_support.png';
 import header_collage from '../../images/home/home_header_collage_2.png';
 
 const openInNewTab = (url: string) => {
@@ -10,21 +9,16 @@ const openInNewTab = (url: string) => {
 };
 
 export const HomeHeader = () => (
-  <div className="home-header">
-    <div className="container">
-      <div className="home-header__support">Direct Community Support <br/> for Artists</div>
-      <div className="home-header__bg-image" style={{backgroundImage: `url(${header_collage}`}}>
-        <div className="row">
-          <div className="col-md-4">
-            <div className="home-header__info">
-            Simple recurring payments directly to music artists on a platform owned by artists.  Own your creative freedom and sustainably continue making art for the people that love it. Join us and become a member-owner of an alternative platform that values artists.
-            </div>
-            <button className="home-header__button btn">Create an Artist Page</button>
-          </div>
-          <button className="home-header__button btn" onClick={() => openInNewTab(config.menuUrls.createArtist)}>
-            Create an Artist Page
-          </button>
+  <div className="home-header container-fluid">
+    <div className="home-header__bg-image row justify-content-center" style={{backgroundImage: `url(${header_collage}`}}>
+    <div className="home-header__support">Direct Community Support for Artists</div>
+      <div className="col-md-6 align-self-end">
+        <div className="home-header__info">
+          Simple recurring payments directly to music artists on a platform owned by artists.  Own your creative freedom and sustainably continue making art for the people that love it. Join us and become a member-owner of an alternative platform that values artists.
         </div>
+        <button className="home-header__button btn" onClick={() => openInNewTab(config.menuUrls.createArtist)}>
+          Create an Artist Page
+        </button>
       </div>
     </div>
   </div>

7ad53a48931b99f335b652add3021d373d1ad86a
Deshon, Ryan <ryandeshon@gmail.com>
2019-05-28 22:00:38 -0400

    Added How Images Hover

--- client/src/containers/home/HomeHow.tsx
+++ client/src/containers/home/HomeHow.tsx
@@ -17,47 +17,41 @@ export const HomeHow = () => (
       </div>
 
       <div className="home-how__list">
-        <div className="row">
-          <div className="col-md-7">
-            <div className="row home-how__list-item phone">
-              <div className="col-2">
-                <div className="home-how__list-item_number">01</div>
-              </div>
-              <div className="col-10">
-                <div className="home-how__list-item_title">
-                  Artists post unique/ unreleased content.
-                </div>
-                <div className="home-how__list-item_copy">
-                Artists on Ampled post things you won’t find anywhere else - like demos, unreleased recordings, access to exclusive merch, discounts, personal notes, announcements - and more.
-                </div>
-              </div>
+        <div className="row home-how__list-item phone">
+          <div className="col-2">
+            <div className="home-how__list-item_number">01</div>
+          </div>
+          <div className="col-md-6">
+            <div className="home-how__list-item_title">
+              Artists post unique/ unreleased content.
             </div>
-            <div className="row home-how__list-item crowd">
-              <div className="col-2">
-                <div className="home-how__list-item_number">02</div>
-              </div>
-              <div className="col-10">
-                <div className="home-how__list-item_title">Artists are supported directly by their community</div>
-                <div className="home-how__list-item_copy">
-                  Artists can be supported directly for $3 or more per month - unlocking access to their exclusive content.
-                </div>
-              </div>
-            </div>
-            <div className="row home-how__list-item money">
-              <div className="col-2">
-                <div className="home-how__list-item_number no-tail">03</div>
-              </div>
-              <div className="col-10">
-                <div className="home-how__list-item_title">Artists collect monthly recurring revenue</div>
-                <div className="home-how__list-item_copy">
-                  When an artist posts something new, their supporters get a notification. 
-                </div>
-              </div>
+            <div className="home-how__list-item_copy">
+            Artists on Ampled post things you won’t find anywhere else - like demos, unreleased recordings, access to exclusive merch, discounts, personal notes, announcements - and more.
             </div>
           </div>
-          <div className="col-md-5">
-            <img className="home-how__list_image phone" src={phone} />
-            <img className="home-how__list_image crowd" src={crowd} />
+          <img className="home-how__list_image phone" src={phone} />
+        </div>
+        <div className="row home-how__list-item crowd">
+          <div className="col-2">
+            <div className="home-how__list-item_number">02</div>
+          </div>
+          <div className="col-md-6">
+            <div className="home-how__list-item_title">Artists are supported directly by their community</div>
+            <div className="home-how__list-item_copy">
+              Artists can be supported directly for $3 or more per month - unlocking access to their exclusive content.
+            </div>
+          </div>
+          <img className="home-how__list_image crowd" src={crowd} />
+        </div>
+        <div className="row home-how__list-item money">
+          <div className="col-2">
+            <div className="home-how__list-item_number no-tail">03</div>
+          </div>
+          <div className="col-md-6">
+            <div className="home-how__list-item_title">Artists collect monthly recurring revenue</div>
+            <div className="home-how__list-item_copy">
+              When an artist posts something new, their supporters get a notification. 
+            </div>
             <img className="home-how__list_image money" src={money} />
           </div>
         </div>

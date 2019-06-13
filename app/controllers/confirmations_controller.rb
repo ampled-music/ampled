72373adead18e3574fee45803ede6fce3f7b500b
Sean Adams <sean@bearcla.ws>
2019-06-13 16:05:03 -0400

    sign in on confirmation

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,4 +1,10 @@
 class ConfirmationsController < Devise::ConfirmationsController
+  def show
+    super do |resource|
+      sign_in(resource) if resource.errors.empty?
+    end
+  end
+
   private
 
   def after_confirmation_path_for(_resource_name, resource)

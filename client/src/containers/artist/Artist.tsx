e253fd5b191c4cbb2f3df70d31ddd635f51509b2
Deshon, Ryan <ryandeshon@gmail.com>
2019-07-13 15:27:17 -0400

    Changes for modal paper tear

--- client/src/containers/artist/Artist.tsx
+++ client/src/containers/artist/Artist.tsx
@@ -195,6 +195,9 @@ class ArtistComponent extends React.Component<Props, any> {
               background-color: ${artist.accent_color};
               color: white;
             }
+            .new-post button {
+              background-color: ${artist.accent_color};
+            }
             .btn.btn-support:hover, .private-support-btn > .btn:hover {
               background-color: ${this.ColorLuminance(artist.accent_color, -0.2)};
             }

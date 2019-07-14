86fd05aec92154103f766b13068d067396b6b390
Benton <benton.anderson@gmail.com>
2019-07-14 17:47:19 -0400

    lint

--- app/models/artist_page.rb
+++ app/models/artist_page.rb
@@ -51,7 +51,7 @@ class ArtistPage < ApplicationRecord
     base = "https://connect.stripe.com/express/oauth/authorize"
     params = {
       redirect_uri: "#{ENV["REACT_APP_API_URL"]}stripe_oauth_callback",
-      client_id: ENV['STRIPE_CLIENT_ID'] || "ca_Eowu0ycKNxFo46f8hqlCNCpt4w26bxer",
+      client_id: ENV["STRIPE_CLIENT_ID"] || "ca_Eowu0ycKNxFo46f8hqlCNCpt4w26bxer",
       state: stripe_state_token,
       "suggested_capabilities[]" => "card_payments"
     }.to_query

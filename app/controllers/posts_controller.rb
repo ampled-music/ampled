9a8da0cad83ae4dcc811a99f60a00cfe84d22d1a
Benton <benton.anderson@gmail.com>
2019-07-14 18:03:08 -0400

    wip proposal for edit endpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    artist_page_id = @Post.artist_page.id
+    @post.update(post_update_params)
+    200
+  end
+
   private
 
   def post_params
@@ -35,4 +41,13 @@ class PostsController < ApplicationController
   def set_post
     @post = Post.find(params[:id])
   end
+
+  def post_update_params
+    params.require(:post).permit(
+      :title,
+      :body,
+      :image_url,
+      :audio_file,
+      :is_private
+    )
 end

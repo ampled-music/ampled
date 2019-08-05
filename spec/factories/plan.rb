96ec95b78c956c72930fb513a39085b7ea56ef0b
Benton <benton.anderson@gmail.com>
2019-08-04 22:09:57 -0400

    move to nominal_amount, better amount-specific specs

--- spec/factories/plan.rb
+++ spec/factories/plan.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :plan do
     artist_page
-    amount { Random.rand(50_000) }
+    nominal_amount { Random.rand(50_000) }
     stripe_id { Faker::Alphanumeric.alphanumeric(10) }
   end
 end

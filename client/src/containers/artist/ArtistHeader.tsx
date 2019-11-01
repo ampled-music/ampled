dfb1d6bf7a77e3a67fbbae1aa1c0e39dcf5779d1
Sean Adams <sean@bearcla.ws>
2019-11-01 15:19:02 -0400

    adjust swipe tolerance

--- client/src/containers/artist/ArtistHeader.tsx
+++ client/src/containers/artist/ArtistHeader.tsx
@@ -200,6 +200,7 @@ export class ArtistHeader extends React.Component<Props, any> {
           onSwipeLeft={this.onSwipeLeft}
           onSwipeRight={this.onSwipeRight}
           allowMouseEvents={true}
+          tolerance={50}
           className="artist-header__photo-container_border_swipe"
         >
         </Swipe>

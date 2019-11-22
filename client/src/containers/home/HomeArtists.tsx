f06427a92e6edb5ff7fc7ad19acd265f773a6a2b
Ryan DeShon <ryandeshon@gmail.com>
2019-11-21 19:36:30 -0500

    Image, link, and name

--- client/src/containers/home/HomeArtists.tsx
+++ client/src/containers/home/HomeArtists.tsx
@@ -88,9 +88,9 @@ class HomeArtistsComponent extends React.Component<Props, State> {
     return (
       <div className="row justify-content-center home-artists__featured">
         <div className="col-md-5">
-          <Link to={`/artist/hardcoreluxury`} className="home-artists__featured_left">
-            <div className="home-artists__featured_title">Hardcore Luxury</div>
-            <img className="home-artists__featured_image" src="https://res.cloudinary.com/ampled-web/image/upload/w_460,h_330,c_crop,g_face/v1572662519/Screen_Shot_2019-10-30_at_6.08.53_PM_v0ymgv.png" />
+          <Link to={`/artist/thanks-light`} className="home-artists__featured_left">
+            <div className="home-artists__featured_title">Thanks Light...</div>
+            <img className="home-artists__featured_image" src="https://res.cloudinary.com/ampled-web/image/upload/w_460,h_300,c_crop,g_face/v1572663505/zanethankslight_hztaz2.jpg" />
             <div className="home-artists__featured_border"></div>
           </Link>
         </div>
@@ -98,7 +98,7 @@ class HomeArtistsComponent extends React.Component<Props, State> {
           <div className="home-artists__featured_info">
             Hardcore Luxury is a rock band from Brooklyn, NY — a new project from Austin Robey and Sean Adams, both founding-members of Ampled. Hardcore Luxury is using Ampled to give their supporters an intimate behind-the-scenes insight into their demo writing and recording process, with colorful commentary, stories, and context.
           </div>
-          <Link to={`/artist/hardcoreluxury`} className="home-artists__featured_link">
+          <Link to={`/artist/thanks-light`} className="home-artists__featured_link">
             Explore Their Page
           </Link> &#10142;
         </div>

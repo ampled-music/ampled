c8a0e4345916817c408ac5c3014da3e492c3213b
Sean Adams <sean@bearcla.ws>
2019-12-03 19:23:35 -0500

    update sentry environment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,5 +3,6 @@ Raven.configure do |config|
     config.async = lambda { |event|
       SentryJob.perform_later(event)
     }
+    config.environment = ENV['RAILS_ENV']
     config.excluded_exceptions = [] #+= ['ActionController::RoutingError']
   end

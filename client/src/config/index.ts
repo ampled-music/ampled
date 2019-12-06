f423366d905443e8329a67d026c129286b3fea8c
Ryan DeShon <ryandeshon@gmail.com>
2019-12-06 17:33:03 -0500

    typo

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -5,7 +5,7 @@ export const config = {
     token: 'token',
   },
   menuUrls: {
-    createArtist: process.env.REACT_APP_URL_CREATE_ARTIST || '/create-artisr',
+    createArtist: process.env.REACT_APP_URL_CREATE_ARTIST || '/create-artist',
     blog: process.env.REACT_APP_URL_BLOG || 'https://app.ampled.com/zine',
     about: process.env.REACT_APP_URL_ABOUT || 'https://app.ampled.com/about',
   },

f962571c120d74515c00afb8584069a320030c7a
Ryan DeShon <ryandeshon@gmail.com>
2019-12-06 17:27:55 -0500

    Color override

--- client/src/containers/create-artist/theme.ts
+++ client/src/containers/create-artist/theme.ts
@@ -3,6 +3,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export const theme = createMuiTheme({
     palette: {
         primary: { main: '#1E1E1E' },
+        secondary: { main: '#1E1E1E' },
     },
     overrides: {
         MuiInputBase: {

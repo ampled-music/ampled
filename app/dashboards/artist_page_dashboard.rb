bd8acc34388f1019a9a55b9eaac492d3d4024ce6
Ryan DeShon <ryandeshon@gmail.com>
2020-01-15 13:30:35 -0500

    Disable search for owners in artists pages

--- app/dashboards/artist_page_dashboard.rb
+++ app/dashboards/artist_page_dashboard.rb
@@ -11,7 +11,7 @@ class ArtistPageDashboard < Administrate::BaseDashboard
     "#{artist_page.name} - #{artist_page.id}"
   end
   ATTRIBUTE_TYPES = {
-    owners: Field::HasMany.with_options(class_name: "User", show: %i[email], searchable: true,
+    owners: Field::HasMany.with_options(class_name: "User", show: %i[email], searchable: false,
                                         searchable_field: 'email'),
     images: Field::HasMany.with_options(show: %i[url]),
     id: Field::Number,

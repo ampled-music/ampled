d5a7ea1e69952297b7684b665709558c7dc6856a
Sean Adams <sean@bearcla.ws>
2020-01-19 13:25:21 -0500

    tests

--- spec/features/create_artist_page_spec.rb
+++ spec/features/create_artist_page_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe ArtistPagesController, type: :request do
   let(:user) { create(:user, confirmed_at: Time.current, email: "creator@ampled.com", name: "Creator") }
+  let(:other_artist_page) { create(:artist_page, slug: "test", approved: true) }
 
   let(:create_params) do
     {
@@ -38,6 +39,27 @@ RSpec.describe ArtistPagesController, type: :request do
     }
   end
 
+  let(:update_params) do
+    {
+      artist_page: {
+        name: "Test",
+        slug: "testslug",
+        bio: "About me"
+      },
+      members: [
+        {
+          email: "creator@ampled.com",
+          firstName: "Creator"
+        },
+        {
+          email: "testfriend@ampled.com",
+          firstName: "Friend"
+        }
+      ],
+      images: ["http://ampled-web.herokuapp.com/static/media/ampled_logo_beta.1ce03b01.svg"]
+    }
+  end
+
   context "when creating an artist page" do
     let(:url) { "/artist_pages" }
 
@@ -67,6 +89,33 @@ RSpec.describe ArtistPagesController, type: :request do
       post url, params: create_params
 
       expect(JSON.parse(response.body)["status"]).to eq "error"
+    end
+  end
+
+  context "when updating an artist page" do
+    let(:create_url) { "/artist_pages" }
+    let(:other_url) { "/artist_pages/#{other_artist_page.id}" }
+    let(:artist_page) { create(:artist_page) }
+
+    before do
+      user.owned_pages << artist_page
+      PageOwnership.find_by(user_id: user[:id], artist_page_id: artist_page[:id]).update(role: "admin")
+    end
+
+    before(:each) do
+      sign_in user
+    end
+
+    it "won't edit someone else's page" do
+      put other_url, params: update_params
+
+      expect(JSON.parse(response.body)["message"]).to eq "You don't have that permission."
+    end
+
+    it "will let admins edit their own page" do
+      sign_in user
+      put "/artist_pages/#{artist_page.id}", params: update_params
+      expect(JSON.parse(response.body)["message"]).to eq "Your page has been updated!"
     end
   end
 end

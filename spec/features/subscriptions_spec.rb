0ef2341050576c1c6300435782a711c969e74d4b
Sean Adams <sean@bearcla.ws>
2020-01-20 18:46:32 -0500

    card update test

--- spec/features/subscriptions_spec.rb
+++ spec/features/subscriptions_spec.rb
@@ -42,6 +42,16 @@ RSpec.describe SubscriptionsController, :vcr, type: :request do
     end
   end
 
+  context "when updating card on file" do
+    before do
+      put "/me/updatecard.json", params: { token: "tok_visa" }
+    end
+
+    it "returns updated card info" do
+      expect(JSON.parse(response.body)["card_last4"]).not_to be_empty
+    end
+  end
+
   context "when creating a subscription" do
     let(:url) { "/subscriptions/" }
 

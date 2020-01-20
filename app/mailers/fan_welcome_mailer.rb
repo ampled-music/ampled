7267e07471fa91d5c215402b2cc870d4a4548331
Sean Adams <sean@bearcla.ws>
2020-01-20 11:45:42 -0500

    per-environment email links

--- app/mailers/fan_welcome_mailer.rb
+++ app/mailers/fan_welcome_mailer.rb
@@ -2,11 +2,11 @@ class FanWelcomeMailer < PostmarkMailer
   # method name must match alias on postmark
   def fan_welcome(user)
     self.template_model = {
-      homepage_url: "http://ampled.com",
-      login_url: "http://ampled.com",
+      homepage_url: ENV["REACT_APP_API_URL"],
+      login_url: ENV["REACT_APP_API_URL"],
       fan_email_address: user.email,
       support_email: ENV["POSTMARK_FROM_EMAIL"],
-      ampled_membership_url: "http://ampled.com",
+      ampled_membership_url: ENV["REACT_APP_API_URL"],
       first_name: user.name
     }
 

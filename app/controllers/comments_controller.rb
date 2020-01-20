34e70928c186bdad7e27a6c7fd42811249ea2570
Sean Adams <sean@bearcla.ws>
2020-01-20 17:26:14 -0500

    comment deletion policy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[destroy]
+  before_action :check_update, only: %i[destroy]
 
   def create
     @comment = Comment.new(comment_params)
@@ -17,6 +18,18 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def render_not_allowed
+    render json: { status: "error", message: "Not allowed." }
+  end
+
+  def check_permission
+    render_not_allowed if current_user.nil?
+
+    return if @comment.user == current_user
+
+    render_not_allowed unless PostPolicy.new(current_user, @comment.post).destroy?
+  end
 
   def comment_params
     params.require(:comment).permit(:text, :post_id).merge(user_id: current_user.id)

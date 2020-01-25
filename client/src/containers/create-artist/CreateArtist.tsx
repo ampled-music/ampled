e38e76f43843aac7415b1c510124101fa5414ed0
Ryan DeShon <ryandeshon@gmail.com>
2020-01-25 14:46:35 -0500

    Clean up

--- client/src/containers/create-artist/CreateArtist.tsx
+++ client/src/containers/create-artist/CreateArtist.tsx
@@ -594,7 +594,6 @@ class CreateArtist extends React.Component<CreateArtistProps, any> {
     } else {
       this.setState({ [name]: value });
     }
-    console.log(name, value);
   };
 
   renderHeader = () => {

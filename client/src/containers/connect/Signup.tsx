2a8895fc86aaac9fdf62a3f97d374f8f8eb14850
Ryan DeShon <ryandeshon@gmail.com>
2020-01-26 18:05:48 -0500

    lint

--- client/src/containers/connect/Signup.tsx
+++ client/src/containers/connect/Signup.tsx
@@ -3,7 +3,10 @@ import './login.scss';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { closeAuthModalAction, openAuthModalAction } from '../../redux/authentication/authentication-modal';
+import {
+  closeAuthModalAction,
+  openAuthModalAction,
+} from '../../redux/authentication/authentication-modal';
 import { Store } from '../../redux/configure-store';
 import * as store from 'store';
 import { signupAction } from '../../redux/signup/signup';
@@ -46,20 +49,32 @@ class SignupComponent extends React.Component<Props, any> {
   state = this.initialState;
   emailPass = {
     email: null,
-    password: null
+    password: null,
   };
 
   componentDidUpdate() {
     const { signup, authentication } = this.props;
 
-    if (this.state.submitted && !signup.errors && authentication.authModalOpen && !authentication.authenticating) {
-      showToastMessage("Signed up! Please wait while we log you in.", MessageType.SUCCESS, { timeOut: 8000 });
+    if (
+      this.state.submitted &&
+      !signup.errors &&
+      authentication.authModalOpen &&
+      !authentication.authenticating
+    ) {
+      showToastMessage(
+        'Signed up! Please wait while we log you in.',
+        MessageType.SUCCESS,
+        { timeOut: 8000 },
+      );
       this.doLogin();
     }
   }
 
   login = async () => {
-    const { token } = await this.props.login(this.emailPass.email, this.emailPass.password);
+    const { token } = await this.props.login(
+      this.emailPass.email,
+      this.emailPass.password,
+    );
     if (token) {
       store.set('token', token);
       this.props.closeAuthModal();
@@ -67,9 +82,9 @@ class SignupComponent extends React.Component<Props, any> {
         window.location = this.props.authentication.redirectTo;
       }
     }
-  }
+  };
 
-  doLogin = once(this.login)
+  doLogin = once(this.login);
 
   passwordsMatch(): boolean {
     const { password, confirmPassword } = this.state;
@@ -102,19 +117,34 @@ class SignupComponent extends React.Component<Props, any> {
     const { email, password, confirmPassword, name, last_name } = this.state;
     this.emailPass = { email, password };
 
-    const submitResult = await this.props.signup(email, password, confirmPassword, name, last_name);
+    const submitResult = await this.props.signup(
+      email,
+      password,
+      confirmPassword,
+      name,
+      last_name,
+    );
 
     if (submitResult) {
       this.setState({ submitted: true });
       this.checkErrors();
     } else {
-      showToastMessage("Error signing up. Maybe you already have an account?", MessageType.ERROR, {timeOut: 8000});
+      showToastMessage(
+        'Error signing up. Maybe you already have an account?',
+        MessageType.ERROR,
+        { timeOut: 8000 },
+      );
     }
   };
 
   handleChange = (e) => {
     const { name, value } = e.target;
-    this.setState({ [name]: value, matchPasswordsError: false, passwordError: null, emailError: null });
+    this.setState({
+      [name]: value,
+      matchPasswordsError: false,
+      passwordError: null,
+      emailError: null,
+    });
   };
 
   toggle = () => {
@@ -143,19 +173,32 @@ class SignupComponent extends React.Component<Props, any> {
         <img className="tear tear__topper" src={tear} alt="" />
         <div className="login">
           <h4>SIGN UP</h4>
-          {authentication.showSupportMessage && authentication.showSupportMessage === 'post' && authentication.artistName && (
-            <p>This is a private post. Sign up and become a supporter of {authentication.artistName} to access it.</p>
-          )}
-          {authentication.showSupportMessage && authentication.showSupportMessage === 'artist' && authentication.artistName && (
-            <p>Sign up to become a supporter of {authentication.artistName}.</p>
-          )}
-          <form className="form-container form-control flex-column" name="login" onSubmit={this.handleSubmit}>
+          {authentication.showSupportMessage &&
+            authentication.showSupportMessage === 'post' &&
+            authentication.artistName && (
+              <p>
+                This is a private post. Sign up and become a supporter of{' '}
+                {authentication.artistName} to access it.
+              </p>
+            )}
+          {authentication.showSupportMessage &&
+            authentication.showSupportMessage === 'artist' &&
+            authentication.artistName && (
+              <p>
+                Sign up to become a supporter of {authentication.artistName}.
+              </p>
+            )}
+          <form
+            className="form-container form-control flex-column"
+            name="login"
+            onSubmit={this.handleSubmit}
+          >
             <input
               className="input-group-text"
               type="text"
               placeholder="First Name"
               name="name"
-              aria-label="First name" 
+              aria-label="First name"
               onChange={this.handleChange}
               required
             />
@@ -164,7 +207,7 @@ class SignupComponent extends React.Component<Props, any> {
               type="text"
               placeholder="Last Name (Optional)"
               name="last_name"
-              aria-label="Last name (optional)" 
+              aria-label="Last name (optional)"
               onChange={this.handleChange}
             />
 
@@ -189,7 +232,9 @@ class SignupComponent extends React.Component<Props, any> {
               required
               minLength={6}
             />
-            <span className="error-message">{(matchPasswordsError && passwordErrorMessage) || passwordError}</span>
+            <span className="error-message">
+              {(matchPasswordsError && passwordErrorMessage) || passwordError}
+            </span>
 
             <input
               className="input-group-text"
@@ -201,12 +246,24 @@ class SignupComponent extends React.Component<Props, any> {
               required
               minLength={6}
             />
-            <span className="error-message">{(matchPasswordsError && passwordErrorMessage) || passwordError}</span>
+            <span className="error-message">
+              {(matchPasswordsError && passwordErrorMessage) || passwordError}
+            </span>
 
             <label className="terms">
-              <input type="checkbox" name="terms" onChange={this.toggle} required aria-label="Accept terms of use" />
+              <input
+                type="checkbox"
+                name="terms"
+                onChange={this.toggle}
+                required
+                aria-label="Accept terms of use"
+              />
               You agree to our{' '}
-              <a href="https://app.ampled.com/terms-of-use" rel="noopener noreferrer" target="_blank">
+              <a
+                href="https://app.ampled.com/terms-of-use"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
                 <u>Terms of Service</u>
               </a>
               .
@@ -217,7 +274,15 @@ class SignupComponent extends React.Component<Props, any> {
           </form>
           <label>
             Already have an account?{' '}
-            <button className="link" onClick={() => this.props.openAuthModal({ modalPage: 'login', redirectTo: this.props.authentication.redirectTo })}>
+            <button
+              className="link"
+              onClick={() =>
+                this.props.openAuthModal({
+                  modalPage: 'login',
+                  redirectTo: this.props.authentication.redirectTo,
+                })
+              }
+            >
               <u>Log in</u>
             </button>
             .
@@ -241,9 +306,6 @@ const mapDispatchToProps = (dispatch) => ({
   closeAuthModal: bindActionCreators(closeAuthModalAction, dispatch),
 });
 
-const Signup = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(SignupComponent);
+const Signup = connect(mapStateToProps, mapDispatchToProps)(SignupComponent);
 
 export { Signup };

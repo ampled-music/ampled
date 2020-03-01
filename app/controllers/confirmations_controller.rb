ef8ccfdca5ed60f707cf881ce0e87d4782526694
Sean Adams <sean@bearcla.ws>
2020-03-01 14:51:55 -0500

    redirect on confirm

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,10 +1,29 @@
 class ConfirmationsController < Devise::ConfirmationsController
   respond_to :json
 
+  # GET /resource/confirmation?confirmation_token=abcdef
+  def show
+    # respond_to :html
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      sign_in(resource_name, resource)
+      redirect_to after_confirmation_path_for(resource_name, resource)
+    else
+      redirect_to after_confirmation_path_for(resource_name, resource)
+    end
+  end
+
   private
 
   def after_confirmation_path_for(_resource_name, resource)
     sign_in(resource)
-    root_path
+    if resource.errors.empty?
+      "#{resource.redirect_uri}?flash=confirmed"
+    else
+      "#{resource.redirect_uri}?flash=confirmerror"
+    end
   end
 end

6927ee3e934a0e2644f0e100de7b04970ea09d74
Ryan DeShon <ryandeshon@gmail.com>
2020-03-04 19:45:44 -0500

    Strip our random color from Homepage

--- client/src/containers/PublicRoute.tsx
+++ client/src/containers/PublicRoute.tsx
@@ -3,8 +3,14 @@ import { Route } from 'react-router-dom';
 import { Nav } from './shared/nav/Nav';
 
 export const PublicRoute = ({ component: Component, ...rest }) => {
+  const randomColor = () => {
+    const bgColor = ['#e9c7c6', '#eddfbd', '#baddac', '#cae4e7'];
+    return bgColor[Math.floor(Math.random() * bgColor.length)];
+  };
+  const [color] = React.useState(randomColor());
+
   const renderComponent = (props) => {
-    document.body.style.background = 'white';
+    document.body.style.background = color;
 
     return (
       <div className="public-routes">

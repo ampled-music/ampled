2df24f8f625aa9f65aa8e0a84417b461cb416c04
Sean Adams <sean@bearcla.ws>
2020-03-25 18:39:59 -0400

    proxy requests to :5000 locally

---
+++ client/src/setupProxy.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const middleware = createProxyMiddleware({
+  target: 'http://localhost:3000',
+  changeOrigin: true,
+});
+
+module.exports = function(app) {
+  app.use('/*.json', middleware);
+};

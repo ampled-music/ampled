4339e56068f2cf07237c8c4943a3a233145da119
Sean Adams <sean@bearcla.ws>
2020-04-05 15:53:04 -0400

    remove env logging

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -12,11 +12,6 @@ class StripeController < ApplicationController
     redirect_to "/settings?stripesuccess=true"
   end
 
-  def log_environments
-    logger.info "Rails.env: #{Rails.env}"
-    logger.info "RAILS_ENV: #{ENV["RAILS_ENV"]}"
-  end
-
   def webhook
     logger.info "Stripe: Verifying webhook event..."
 
@@ -29,8 +24,6 @@ class StripeController < ApplicationController
     event_type = params[:type]
     event_id = params[:id]
     logger.info "STRIPE EVENT: #{event_type} #{event_id} for #{connect_account} (live mode: #{params[:livemode]})"
-
-    log_environments
 
     # Webhooks for Connect may send test data to live endpoints, so we need
     # to ignore test data in production

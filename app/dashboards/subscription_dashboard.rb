218081f22d55aaecf4101cac332d13c9e963940a
Sean Adams <sean@bearcla.ws>
2020-04-08 11:20:44 -0400

    "destroy" sub in backoffice cancels

--- app/dashboards/subscription_dashboard.rb
+++ app/dashboards/subscription_dashboard.rb
@@ -43,7 +43,6 @@ class SubscriptionDashboard < Administrate::BaseDashboard
   FORM_ATTRIBUTES = [
     :user,
     :artist_page,
-    :status,
   ].freeze
 
   # Overwrite this method to customize how subscriptions are displayed

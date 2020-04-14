af734bcd4b3f6bbf707d2ed5fded4cd3b25ce07d
Ana Ulin <ana@ulin.org>
2020-04-13 17:22:21 -0700

    Add migration to set a public_id based on existing URLs (only if public_id is missing).

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_003754) do
+ActiveRecord::Schema.define(version: 2020_04_13_230338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"

16a71ae2fa29943322aa02fabf566231d869780d
Sean Adams <sean@bearcla.ws>
2020-04-14 15:56:32 -0400

    meta tag cleanup

--- app/controllers/react_controller.rb
+++ app/controllers/react_controller.rb
@@ -10,18 +10,25 @@ class ReactController < ActionController::Base
     response_html = render_to_string file: "public/index.html", layout: false
 
     image = artist_page.images.first
+    bio = artist_page.bio.gsub(/[\n\r]/, " ")
 
     # replace title tag and title metas
     response_html.gsub!(/Ampled\s\|\s/, "#{artist_page.name} | Ampled | ")
 
     # replace description metas
-    response_html.gsub!(/content="Ampled allows[^"]+"/, "content=\"#{artist_page.bio}\"")
+    response_html.gsub!(/content="Ampled allows[^"]+"/, "content=\"#{bio}\"")
 
     # additional meta tags not already in index.html
-    additional_meta = "\n<meta name=\"twitter:card\" content=\"summary_large_image\" /> \
-    \n<meta name=\"og:description\" content=\"#{artist_page.bio}\" />"
+    additional_meta = "\n<meta name=\"twitter:card\" content=\"summary_large_image\" />\n \
+    <meta name=\"og:type\" content=\"profile\" />\n \
+    <meta name=\"og:description\" content=\"#{bio}\" />\n \
+    <meta property=\"og:description\" content=\"#{bio}\" />\n"
+
     # add image if one exists
-    additional_meta += "\n<meta property=\"og:image\" content=\"#{image.url}\" />" unless image.nil?
+    unless image.nil?
+      additional_meta += "\n<meta property=\"og:image\" content=\"#{image.url}\" />"
+      additional_meta += "\n<meta name=\"og:image\" content=\"#{image.url}\" />"
+    end
     # add twitter handle if one exists
     if artist_page.twitter_handle.present?
       additional_meta += "\n<meta property=\"twitter:creator\" content=\"@#{artist_page.twitter_handle}\" />"

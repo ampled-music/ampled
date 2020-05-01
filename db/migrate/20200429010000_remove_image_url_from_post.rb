eb28091e22d838a236c176df8e3f8f8613a4ea44
Ana Ulin <ana@ulin.org>
2020-04-30 17:42:58 -0700

    Make migration resilient to empty URLs, which we appear to have at least one of.

---
+++ db/migrate/20200429010000_remove_image_url_from_post.rb
@@ -0,0 +1,33 @@
+class RemoveImageUrlFromPost < ActiveRecord::Migration[5.2]
+  def up
+    migrate_post_image_urls_to_images
+    remove_column :posts, :image_url
+  end
+
+  def down
+    add_column :posts, :image_url, :string
+    migrate_images_url_to_post_image_urls
+  end
+
+  private
+
+  def migrate_post_image_urls_to_images
+    Post.where.not(image_url: nil).each do |post|
+      # Ignore empty-but-not-nil URLs (these are not real images, obvi).
+      next if post.image_url.blank?
+
+      post.update!(images_attributes: [image_attributes(post.image_url)])
+    end
+  end
+
+  def migrate_images_url_to_post_image_urls
+    Image.where(imageable_type: 'Post').each do |image|
+      image.imageable.update!(image_url: image.url)
+      image.destroy!
+    end
+  end
+
+  def image_attributes(url)
+    {url: url, public_id: url.split('/').last.split('.').first}
+  end
+end

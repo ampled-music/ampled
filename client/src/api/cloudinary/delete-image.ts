40e5cf884242793c3f12e4730067d85e8f6c07fd
Sean Adams <sean@bearcla.ws>
2020-05-08 17:03:50 -0400

    catch cloudinary delete errors

--- client/src/api/cloudinary/delete-image.ts
+++ client/src/api/cloudinary/delete-image.ts
@@ -2,14 +2,18 @@ import axios from 'axios';
 import { config } from '../../config';
 
 export const deleteFileFromCloudinary = async (deleteToken: string) => {
-  await axios({
-    method: 'post',
-    url: config.cloudinary.deleteImageUrl,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: {
-      token: deleteToken,
-    },
-  });
+  try {
+    await axios({
+      method: 'post',
+      url: config.cloudinary.deleteImageUrl,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        token: deleteToken,
+      },
+    });
+  } catch (e) {
+    console.log('Removing from Cloudinary failed.');
+  }
 };

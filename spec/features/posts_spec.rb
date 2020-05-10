234e06bcbac15e7ba0962b63b0fb4916ebea4265
Ari <ari.ariel.farias@gmail.com>
2020-05-10 16:34:58 -0400

    fix ruby specs, continued...

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -77,27 +77,46 @@ RSpec.describe "DELETE /posts", type: :request do
     end
   end
 
-  context "when post has audio", vcr: true do
+  context "when post has audio" do
     let(:user) { create(:user) }
-    let(:post) { create(:post, user: user) }
+    let(:post) { create(:post, user: user)}
+    let(:s3_bucket) { double }
+    let(:s3_object) { double }
 
     before(:each) do
       sign_in user
+
+      audio_processing_service = double
+      allow(AudioProcessingService).to receive(:new) { audio_processing_service }
+      allow(audio_processing_service).to receive(:generate_hash) { "fake_hash" }
+      allow(audio_processing_service).to receive(:get_duration) { 123 }
+      allow(audio_processing_service).to receive(:generate_waveform) { [1] }
+      allow(audio_processing_service).to receive(:dispose)
+
+      post.audio_uploads << AudioUpload.new(public_id: "abc.mp3")
+      post.save!
+
+      s3 = double
+      allow(Aws::S3::Resource).to receive(:new) { s3 }
+      allow(s3).to receive(:bucket) { s3_bucket }
+      allow(s3_bucket).to receive(:object) { s3_object }
+      allow(s3_object).to receive(:delete)
+
+      delete "/posts/#{post.id}"
     end
 
     it "returns 200" do
-      post.audio_uploads << AudioUpload.new(public_id: "263b6426-4414-4585-a019-adc62f075a9f.mp3")
-      post.save!
-      delete "/posts/#{post.id}"
       expect(response.status).to eq 200
     end
 
     it "deletes the audio upload record too" do
-      post.audio_uploads << AudioUpload.new(public_id: "35e5b2f2-73e8-451c-be41-51f750f653e4.mp3")
-      post.save!
-      delete "/posts/#{post.id}"
       audio_upload = AudioUpload.where(post_id: post.id).first
       expect(audio_upload).to eq nil
+    end
+
+    it "deletes the audio object from S3" do
+      expect(s3_bucket).to have_received(:object).with("abc.mp3")
+      expect(s3_object).to have_received(:delete)
     end
   end
 end
@@ -137,10 +156,27 @@ RSpec.describe "PUT /posts", type: :request do
 
   context "when a post's audio upload is set to be destroyed", vcr: true do
     let(:user) { create(:user) }
-    let(:post) { create(:post, user: user, audio_uploads: [AudioUpload.new(public_id: "abc.mp3")]) }
+    let(:post) { create(:post, user: user) }
+    let(:s3_bucket) { double }
+    let(:s3_object) { double }
 
     before(:each) do
       sign_in user
+
+      s3 = double
+      allow(Aws::S3::Resource).to receive(:new) { s3 }
+      allow(s3).to receive(:bucket) { s3_bucket }
+      allow(s3_bucket).to receive(:object) { s3_object }
+      allow(s3_object).to receive(:delete)
+
+      audio_processing_service = double
+      allow(AudioProcessingService).to receive(:new) { audio_processing_service }
+      allow(audio_processing_service).to receive(:generate_hash) { "fake_hash" }
+      allow(audio_processing_service).to receive(:get_duration) { 123 }
+      allow(audio_processing_service).to receive(:generate_waveform) { [0] }
+      allow(audio_processing_service).to receive(:dispose)
+
+      post.audio_uploads << AudioUpload.new(public_id: "abc.mp3")
     end
 
     before do
@@ -156,6 +192,11 @@ RSpec.describe "PUT /posts", type: :request do
       audio_upload = AudioUpload.where(post_id: post.id).first
       expect(audio_upload).to eq nil
     end
+
+    it "deletes the audio object from S3" do
+      expect(s3_bucket).to have_received(:object).with("abc.mp3")
+      expect(s3_object).to have_received(:delete)
+    end
   end
 
   context "when a post has a new audio upload" do
@@ -164,6 +205,13 @@ RSpec.describe "PUT /posts", type: :request do
 
     before(:each) do
       sign_in user
+
+      audio_processing_service = double
+      allow(AudioProcessingService).to receive(:new) { audio_processing_service }
+      allow(audio_processing_service).to receive(:generate_hash) { "fake_hash" }
+      allow(audio_processing_service).to receive(:get_duration) { 123 }
+      allow(audio_processing_service).to receive(:generate_waveform) { [0] }
+      allow(audio_processing_service).to receive(:dispose)
     end
 
     before { put "/posts/#{post.id}", params: { post: { audio_uploads_attributes: [{ public_id: "abc.mp3" }] } } }
@@ -237,7 +285,12 @@ RSpec.describe "POST /posts", type: :request do
     end
   end
 
-  context "when the post has audio", vcr: true do
+  context "when the post has audio" do
+    let(:hash_key) { "fake_hash" }
+    let(:duration) { 110 }
+    let(:waveform) { [20, 55, 23, 2, 80] }
+    let(:audio_processing_service) { double }
+
     before do
       owner_user.owned_pages << artist_page
       sign_in owner_user
@@ -248,10 +301,16 @@ RSpec.describe "POST /posts", type: :request do
           title: "test",
           body: "test test",
           audio_uploads_attributes: [{
-            public_id: "c9598952-0a29-4ce9-88cd-e33591d8ebda.mp3"
+            public_id: "abc.mp3"
           }]
         }
       }
+
+      allow(AudioProcessingService).to receive(:new) { audio_processing_service }
+      allow(audio_processing_service).to receive(:generate_hash) { hash_key }
+      allow(audio_processing_service).to receive(:get_duration) { duration }
+      allow(audio_processing_service).to receive(:generate_waveform) { waveform }
+      allow(audio_processing_service).to receive(:dispose)
 
       post "/artist_pages/#{artist_page.id}/posts", params: post_params
     end
@@ -262,7 +321,31 @@ RSpec.describe "POST /posts", type: :request do
 
     it "should create audio upload record" do
       get "/artist_pages/#{artist_page.id}.json"
-      expect(JSON.parse(response.body)["posts"][0]["audio_uploads"][0]["public_id"]).to eq("c9598952-0a29-4ce9-88cd-e33591d8ebda.mp3")
+      expect(JSON.parse(response.body)["posts"][0]["audio_uploads"][0]["public_id"]).to eq("abc.mp3")
+    end
+
+    it "should have audio processing service generate hash_key" do
+      expect(AudioProcessingService).to have_received(:new).with("abc.mp3")
+      expect(audio_processing_service).to have_received(:generate_hash)
+
+      get "/artist_pages/#{artist_page.id}.json"
+      expect(JSON.parse(response.body)["posts"][0]["audio_uploads"][0]["hash_key"]).to eq(hash_key)
+    end
+
+    it "should get duration from audio processing service" do
+      expect(AudioProcessingService).to have_received(:new).with("abc.mp3")
+      expect(audio_processing_service).to have_received(:get_duration)
+
+      get "/artist_pages/#{artist_page.id}.json"
+      expect(JSON.parse(response.body)["posts"][0]["audio_uploads"][0]["duration"]).to eq(duration)
+    end
+
+    it "should have audio processing service generate waveform" do
+      expect(AudioProcessingService).to have_received(:new).with("abc.mp3")
+      expect(audio_processing_service).to have_received(:generate_waveform)
+
+      get "/artist_pages/#{artist_page.id}.json"
+      expect(JSON.parse(response.body)["posts"][0]["audio_uploads"][0]["waveform"]).to eq(waveform)
     end
   end
 end
@@ -274,23 +357,16 @@ RSpec.describe "Download posts", :vcr, type: :request do
   let(:public_user) { create(:user) }
   let(:artist_page) { create(:artist_page, approved: true, slug: "test") }
   let(:public_download_post) do
-    create(:post, artist_page_id: artist_page.id, title: "test",
-           body: "test test", allow_download: true,
-           audio_uploads: [AudioUpload.new(public_id: "c9598952-0a29-4ce9-88cd-e33591d8ebda.mp3")])
+    create(:post, artist_page_id: artist_page.id, title: "test", body: "test test", allow_download: true)
   end
   let(:private_download_post) do
-    create(:post, artist_page_id: artist_page.id, title: "test",
-           body: "test test", allow_download: true, is_private: true,
-           audio_uploads: [AudioUpload.new(public_id: "c9598952-0a29-4ce9-88cd-e33591d8ebda.mp3")])
+    create(:post, artist_page_id: artist_page.id, title: "test", body: "test test", allow_download: true, is_private: true)
   end
   let(:public_no_download_post) do
-    create(:post, artist_page_id: artist_page.id, title: "test",
-           body: "test test", allow_download: false,
-           audio_uploads: [AudioUpload.new(public_id: "c9598952-0a29-4ce9-88cd-e33591d8ebda.mp3")])
+    create(:post, artist_page_id: artist_page.id, title: "test", body: "test test", allow_download: false)
   end
   let(:public_no_audio_post) do
-    create(:post, artist_page_id: artist_page.id, title: "test",
-           body: "test test", allow_download: true, is_private: true)
+    create(:post, artist_page_id: artist_page.id, title: "test", body: "test test", allow_download: true, is_private: true)
   end
   let(:create_sub_url) { "/subscriptions/" }
 
@@ -301,6 +377,19 @@ RSpec.describe "Download posts", :vcr, type: :request do
     }
   end
   let(:existing_stripe_auth) { JSON.parse(File.read("stripe_account_stub.json")) }
+
+  before(:each) do
+    audio_processing_service = double
+    allow(AudioProcessingService).to receive(:new) { audio_processing_service }
+    allow(audio_processing_service).to receive(:generate_hash) { "fake_hash" }
+    allow(audio_processing_service).to receive(:get_duration) { 123 }
+    allow(audio_processing_service).to receive(:generate_waveform) { [0] }
+    allow(audio_processing_service).to receive(:dispose)
+
+    public_download_post.audio_uploads << AudioUpload.new(public_id: "abc.mp3")
+    private_download_post.audio_uploads << AudioUpload.new(public_id: "def.mp3")
+    public_no_download_post.audio_uploads << AudioUpload.new(public_id: "ghi.mp3")
+  end
 
   context "when user is unauthenticated" do
     context "downloading a public downloadable post" do
@@ -484,13 +573,11 @@ RSpec.describe "GET /slug/:artist/post/:postid", :vcr, type: :request do
   let(:artist_page) { create(:artist_page, approved: true, slug: "test") }
   let(:public_download_post) do
     create(:post, artist_page_id: artist_page.id, title: "test",
-           body: "test test", allow_download: true,
-           audio_file: "62278a79-1221-4f5a-85b3-9c21af6ffbf8.")
+           body: "test test", allow_download: true)
   end
   let(:private_download_post) do
     create(:post, artist_page_id: artist_page.id, title: "test",
-           body: "test test", allow_download: true, is_private: true,
-           audio_file: "62278a79-1221-4f5a-85b3-9c21af6ffbf8.")
+           body: "test test", allow_download: true, is_private: true)
   end
   let(:create_sub_url) { "/subscriptions/" }
 
@@ -501,6 +588,18 @@ RSpec.describe "GET /slug/:artist/post/:postid", :vcr, type: :request do
     }
   end
   let(:existing_stripe_auth) { JSON.parse(File.read("stripe_account_stub.json")) }
+
+  before(:each) do
+    audio_processing_service = double
+    allow(AudioProcessingService).to receive(:new) { audio_processing_service }
+    allow(audio_processing_service).to receive(:generate_hash) { "fake_hash" }
+    allow(audio_processing_service).to receive(:get_duration) { 123 }
+    allow(audio_processing_service).to receive(:generate_waveform) { [0] }
+    allow(audio_processing_service).to receive(:dispose)
+
+    public_download_post.audio_uploads << AudioUpload.new(public_id: "abc.mp3")
+    private_download_post.audio_uploads << AudioUpload.new(public_id: "def.mp3")
+  end
 
   context "when user is unauthenticated" do
     context "viewing a public post" do

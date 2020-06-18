82cba9bedc12593e740e8d721722a63e7dbd4c53
Ryan DeShon <ryandeshon@gmail.com>
2020-06-18 19:25:08 -0400

    Disable backdrop close

--- client/src/containers/artist/posts/post/Post.tsx
+++ client/src/containers/artist/posts/post/Post.tsx
@@ -571,6 +571,7 @@ class PostComponent extends React.Component<any, any> {
           open={this.state.showEditPostModal}
           onClose={this.closeEditPostModal}
           className="post-modal"
+          disableBgClose={true}
         >
           <PostForm
             close={this.closeEditPostModal}

7f4c72f90d569ba495d5dec0de15510d665c5c5f
Ryan DeShon <ryandeshon@gmail.com>
2020-07-06 16:59:35 -0400

    Fix location

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -9,7 +9,7 @@ export const config = {
   menuUrls: {
     createArtist: process.env.REACT_APP_URL_CREATE_ARTIST || '/create-artist',
     blog: process.env.REACT_APP_URL_BLOG || '/blog',
-    about: process.env.REACT_APP_URL_ABOUT || '/page/about',
+    about: process.env.REACT_APP_URL_ABOUT || '/page/about-us',
   },
   cloudinary: {
     uploadImageUrl: 'https://api.cloudinary.com/v1_1/ampled-web/upload',

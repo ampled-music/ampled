b003541feacd8ec38b3fd2119790edf774c05e1f
Ryan DeShon <ryandeshon@gmail.com>
2020-07-11 18:17:33 -0400

    Collins first designs for Blog Posts

--- client/src/containers/blog/BlogPost.tsx
+++ client/src/containers/blog/BlogPost.tsx
@@ -4,6 +4,9 @@ import * as React from 'react';
 
 import { apiAxios } from '../../api/setup-axios';
 import { Loading } from '../shared/loading/Loading';
+import Moment from 'moment';
+import { ReactSVG } from 'react-svg';
+import PhotoIcon from '../../images/icons/Icon_Photo.svg';
 
 interface PostProps {
   match: {
@@ -16,7 +19,15 @@ interface PostProps {
 class BlogPost extends React.Component<PostProps, any> {
   state = {
     title: '',
-    content: '',
+    body: '',
+    author: '',
+    excerpt: '',
+    date: '',
+    featured_image: '',
+    photo_credit: {
+      photo_by: '',
+      site: '',
+    },
     loading: true,
   };
 
@@ -29,14 +40,24 @@ class BlogPost extends React.Component<PostProps, any> {
       this.setState({ loading: true });
       const { data } = await apiAxios({
         method: 'get',
-        url: `http://cms.ampled.com/wp-json/wp/v2/posts?slug=${this.props.match.params.slug}`,
+        url: `http://cms.ampled.com/wp-json/wp/v2/posts?slug=${this.props.match.params.slug}&_embed`,
       });
+      const content = data[0];
 
-      if (data[0].slug === this.props.match.params.slug) {
+      if (content.slug === this.props.match.params.slug) {
+        console.log(content);
         this.setState({
           loading: false,
-          title: data[0].title.rendered,
-          content: data[0].content.rendered,
+          title: content.title.rendered,
+          body: content.content.rendered,
+          author: content._embedded.author[0].name,
+          excerpt: content.excerpt.rendered,
+          date: content.date,
+          featured_image: content._embedded['wp:featuredmedia'][0].source_url,
+          photo_credit: {
+            photo_by: content.acf.photo_by,
+            site: content.acf.site,
+          },
         });
       }
     } catch (e) {
@@ -50,16 +71,46 @@ class BlogPost extends React.Component<PostProps, any> {
       return <Loading artistLoading={true} />;
     }
     return (
-      <div className="container post">
-        <div className="row justify-content-center">
-          <div className="col-md-8">
+      <div>
+        <div
+          className="blog-post__featured-image"
+          style={{ backgroundImage: `url(${this.state.featured_image})` }}
+        ></div>
+        <div className="container blog-post">
+          <div className="blog-post__content">
             <h1
-              className="post__title"
+              className="blog-post__title"
               dangerouslySetInnerHTML={{ __html: this.state.title }}
             />
+            {this.state.excerpt && (
+              <div
+                className="blog-post__excerpt"
+                dangerouslySetInnerHTML={{ __html: this.state.excerpt }}
+              />
+            )}
+
+            <div className="blog-post__info">
+              <div className="blog-post__info_author">{this.state.author}</div>
+              <div className="blog-post__info_date">
+                {Moment(this.state.date).format('MMMM do, YYYY')}
+              </div>
+            </div>
+
+            {this.state.photo_credit.photo_by && (
+              <div className="blog-post__photo_credit">
+                <ReactSVG className="icon icon_black" src={PhotoIcon} />
+                {this.state.photo_credit.site ? (
+                  <a href={this.state.photo_credit.site}>
+                    {this.state.photo_credit.photo_by}
+                  </a>
+                ) : (
+                  this.state.photo_credit.photo_by
+                )}
+              </div>
+            )}
             <div
-              className="post__content"
-              dangerouslySetInnerHTML={{ __html: this.state.content }}
+              className="blog-post__body"
+              dangerouslySetInnerHTML={{ __html: this.state.body }}
             />
           </div>
         </div>

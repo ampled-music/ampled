c6fce0caa0c7284eb3ac3143eeadfd7d685f6386
Ryan DeShon <ryandeshon@gmail.com>
2020-07-30 14:28:55 -0400

    Move social after other social columns

--- db/migrate/20200730172123_add_external_soical.rb
+++ db/migrate/20200730172123_add_external_soical.rb
@@ -1,7 +1,7 @@
 class AddExternalSoical < ActiveRecord::Migration[5.2]
   def change
-    add_column :artist_pages, :bandcamp_handle, :string
-    add_column :artist_pages, :youtube_handle, :string
-    add_column :artist_pages, :external_handle, :string
+    add_column :artist_pages, :bandcamp_handle, :string, after: :twitter_handle
+    add_column :artist_pages, :youtube_handle, :string, after: :twitter_handle
+    add_column :artist_pages, :external_handle, :string, after: :twitter_handle
   end
 end

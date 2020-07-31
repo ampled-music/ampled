98046372e76c8f32e53ad4caeaf807c371b67bb2
Ryan DeShon <ryandeshon@gmail.com>
2020-07-31 19:04:57 -0400

    Clean up

--- client/src/containers/artist/PromoteArtist.tsx
+++ client/src/containers/artist/PromoteArtist.tsx
@@ -3,11 +3,8 @@ import './artist.scss';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { CreateArtist } from '../create-artist/CreateArtist';
 import { getArtistAction } from '../../redux/artists/get-details';
 import { Store } from '../../redux/configure-store';
-
-import { ArtistModel } from '../../redux/artists/initial-state';
 
 class PromoteArtist extends React.Component<any> {
   componentDidMount() {
@@ -19,33 +16,62 @@ class PromoteArtist extends React.Component<any> {
       promote_facebook_image,
       promote_square_images,
       promote_story_images,
+      supporter_images,
+      name,
     } = this.props.artists.artist;
 
-    console.log(promote_square_images);
+    console.log(this.props.artists.artist);
 
     return (
       <div className="artist-promote container">
-        <h1>Facebook</h1>
+        <h1>Promote {name}</h1>
+        <p>
+          Use these images to promote your page. Remember to tag{' '}
+          <a href="https://www.instagram.com/ampl3d/" target="_blank">
+            @ampl3d
+          </a>{' '}
+          so we can repost.
+        </p>
+        <h2>Banner</h2>
         <div className="row">
           <div className="col-md-8">
-            {promote_facebook_image && <img src={promote_facebook_image} />}
+            {promote_facebook_image && (
+              <a download href={promote_facebook_image}>
+                <img src={promote_facebook_image} />
+              </a>
+            )}
           </div>
         </div>
-        <h1>Instragam Post</h1>
+        <h2>Instragam Post</h2>
         <div className="row">
           {promote_square_images &&
             promote_square_images.map((image) => (
               <div className="col-md-4">
-                <img src={image.url} />
+                <a download={image.name} href={image.url} title={image.name}>
+                  <img src={image.url} />
+                </a>
               </div>
             ))}
         </div>
-        <h1>Instagram Story</h1>
+        <h2>Instagram Story</h2>
         <div className="row">
           {promote_story_images &&
             promote_story_images.map((image) => (
               <div className="col-md-4">
-                <img src={image.url} />
+                <a download={image.name} href={image.url} title={image.name}>
+                  <img src={image.url} />
+                </a>
+              </div>
+            ))}
+        </div>
+        <h2>Supporter</h2>
+        <div className="row">
+          {supporter_images &&
+            supporter_images.map((image) => (
+              <div className="col-md-4">
+                <a download={image.name} href={image.url} title={image.name}>
+                  <img src={image.url} />
+                </a>
               </div>
             ))}
         </div>

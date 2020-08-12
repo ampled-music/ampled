5026aeb8995de202df8939f20f8688cd2f1109a3
Ari Farias <ari.ariel.farias@gmail.com>
2020-08-12 19:38:10 -0400

    Features/610 artist approved email (#616)
    * 610: artist approved email with social image

---
+++ spec/jobs/artist_page_approved_email_job_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+describe ArtistPageApprovedEmailJob, type: :job do
+  let(:social_image) do
+    {
+      name: "socialImg1",
+      url: "social-image.com",
+      description: ""
+    }
+  end
+
+  it "sends emails to the owners of the artist page" do
+    allow(SendBatchEmail).to receive(:call)
+    allow(SocialImages::Images::Square1).to receive(:build) { social_image }
+
+    owner = create(:user)
+    artist = create(:artist_page, name: "Testpage", slug: "test")
+    artist.owners << owner
+
+    described_class.new.perform(artist.id)
+
+    expect(SendBatchEmail).to have_received(:call).with(
+      [
+        {
+          from: ENV["POSTMARK_FROM_EMAIL"],
+          to: owner.email,
+          template_alias: "artist-page-approved",
+          template_model: {
+            app_base_url: ENV["REACT_APP_API_URL"],
+            artist_name: artist.name,
+            artist_slug: artist.slug,
+            social_image_url: social_image[:url]
+          }
+        }
+      ]
+    )
+  end
+end

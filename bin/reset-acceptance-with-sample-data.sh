aca52b19d2f2473e12cecc15d9b296ae114df9bd
Ana Ulin <ana@ulin.org>
2020-09-18 08:39:20 -0700

    (644) Add dummy:admin task to create a well-known admin account (#632)
    * Add dummy:admin task to create a well-known admin account, add tests for rake dummy tasks, clean up unused stuff.
    
    * Update dummy task to use new acceptance Cloudinary account and test images. Fix testing image code to always use matching image url and image public id.
    
    * Add lib/tasks/** to ignored paths for Codecov, because now that I added one spec for a rake task, it has discovered that others are untested and is mad.
    
    * Remove "destroy all" from dummy data creation methods. Users can call the dummy:destroy task insteiad.

--- bin/reset-acceptance-with-sample-data.sh
+++ bin/reset-acceptance-with-sample-data.sh
@@ -9,4 +9,4 @@ set -x
 
 heroku pg:reset DATABASE_URL --confirm $ACCEPTANCE
 heroku run rake db:migrate -a $ACCEPTANCE
-heroku run rake db:seed db:sample_data -a $ACCEPTANCE
+heroku run rake dummy:admin dummy:users dummy:artist_pages dummy:posts -a $ACCEPTANCE

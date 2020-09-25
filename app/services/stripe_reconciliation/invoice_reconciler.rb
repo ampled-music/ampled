f600c19125c5eade6d6aa9c4311aa1f6bee7f941
Ryan Quanz <ryanquanz@gmail.com>
2020-09-24 21:58:00 -0400

    (634) start reconciling Stripe invoices (#634)
    * Add StripeReconciliation::InvoiceReconciler
    
    This service determines if a provided Stripe invoice was expected. A Stripe invoice can be unexpected if it:
    
    1. Has an unexpected currency.
    2. Has a Stripe subscription ID we have no record of.
    3. Has an amount that does not match the plan related to the subscription.
    
    * add StripeReconciliation::AnomalyNotifier
    
    * add StripeReconciliation::ReconcileStripeObjectJob
    
    * reconcile stripe objects from webhooks

---
+++ app/services/stripe_reconciliation/invoice_reconciler.rb
@@ -0,0 +1,33 @@
+module StripeReconciliation
+  class InvoiceReconciler
+    attr_reader :anomaly
+
+    EXPECTED_CURRENCIES = ["usd"].freeze
+
+    def initialize(stripe_invoice)
+      @stripe_invoice = stripe_invoice
+    end
+
+    def reconcile
+      # TODO: Record currency on the Plan and use that to verify
+      unless EXPECTED_CURRENCIES.include?(@stripe_invoice[:currency])
+        @anomaly = :unexpected_currency
+        return false
+      end
+
+      subscription = Subscription.find_by(stripe_id: @stripe_invoice[:subscription])
+
+      if subscription.nil?
+        @anomaly = :subscription_not_found
+        return false
+      end
+
+      if subscription.plan.nominal_amount != @stripe_invoice[:total]
+        @anomaly = :unexpected_amount
+        return false
+      end
+
+      true
+    end
+  end
+end

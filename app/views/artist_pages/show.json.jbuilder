a63789d28f37b9910ad67d141bbea5b92a6681d7
Ryan <ryandeshon@gmail.com>
2020-10-12 16:00:47 -0400

    (689): Community page footer text (#656)
    * Show more supporters
    
    * Desktop footer
    
    * Mobile
    
    * Update links
    
    * Email form

--- app/views/artist_pages/show.json.jbuilder
+++ app/views/artist_pages/show.json.jbuilder
@@ -42,7 +42,7 @@ json.owners @artist_page.page_ownerships do |ownership|
 end
 
 json.supporters @artist_page.active_subscribers.includes(%i[image page_ownerships owned_pages])
-  .shuffle.take(20), partial: "users/user", as: :user
+  .shuffle.take(25), partial: "users/user", as: :user
 
 @expand_artist = false
 
--- client/src/containers/shared/footer/Footer.tsx
+++ client/src/containers/shared/footer/Footer.tsx
@@ -4,7 +4,8 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { config } from '../../../config';
 
-import logo from '../../../images/ampled_logo.svg';
+import logo from '../../../images/ampled_coop_logo.svg';
+import fist from '../../../images/icons/Icon_Fist.svg';
 
 interface Props {
   match: {
@@ -17,6 +18,7 @@ interface Props {
 }
 
 class Footer extends React.Component<Props, any> {
+  // const bodyBg = document.body.style.backgroundColor;
   render() {
     if (
       this.props.match.path.indexOf('/create-artist') === 0 ||
@@ -26,68 +28,104 @@ class Footer extends React.Component<Props, any> {
     } else {
       return (
         <footer id="footer">
-          <div className="footer container">
-            <div className="footer__email">
-              <div className="title">Join Our Mailing List</div>
-              <div className="email input-group mb-3">
-                <form
-                  action="https://ampled.us19.list-manage.com/subscribe/post?u=514372f571f3cb5abdf8a2637&amp;id=50f2ab4389"
-                  method="post"
-                >
-                  <input
-                    className="form-control"
-                    type="email"
-                    name="EMAIL"
-                    aria-label="Email Address"
-                    placeholder="Email Address"
-                  />
-                  <div className="input-group-append">
-                    <input
-                      type="submit"
-                      value="Gimme &rarr;"
-                      name="subscribe"
-                      id="mc-embedded-subscribe"
-                      className="btn btn__dark"
-                    />
+          <div className="footer">
+            <div className="footer__top container">
+              <div className="footer__top_left">
+                <div className="footer__info">
+                  <div className="title">The Co-Op</div>
+                  <a href="/page/about-us">About us</a>
+                  <a href="/page/who-we-are">Who are we</a>
+                  <a href="https://docs.ampled.com/coop/">How it works</a>
+                  <a href="/page/open-dashboard">Transparency</a>
+                  <a href="/page/faq">FAQs</a>
+                </div>
+                <div className="footer__info">
+                  <div className="title">Get Involved</div>
+                  <a href={config.menuUrls.createArtist}>Create Artist Page</a>
+                  <a href="/page/join-the-co-op">Ways to join</a>
+                  <a href="/page/become-a-contributor">Become a worker</a>
+                  <a href="/page/how-to-help">How to help</a>
+                  <a href="/page/press">Press</a>
+                </div>
+                <div className="footer__info">
+                  <div className="title">Get Connected</div>
+                  <a href="/page/contact">Contact us</a>
+                  <a href="/page/events">Events</a>
+                  <a href="/blog/">Blog</a>
+                  <a
+                    href="https://www.instagram.com/ampl3d"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Instagram
+                  </a>
+                  <a
+                    href="https://twitter.com/ampl3d"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Twitter
+                  </a>
+                </div>
+              </div>
+              <div className="footer__top_right">
+                <div className="footer__top_right_logo-btn">
+                  <img src={logo} className="coop-logo" alt="Ampled Co-Op" />
+                  <button
+                    className="btn btn-join-ampled"
+                    style={{
+                      backgroundColor: document.body.style.backgroundColor,
+                    }}
+                  >
+                    Join the Ampled community!
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="footer__bottom">
+              <div className="container footer__bottom_flex">
+                <div className="footer__bottom_left">
+                  <div className="title">Subscribe to our mailing list</div>
+                  <div className="email input-group">
+                    <form
+                      action="https://ampled.us19.list-manage.com/subscribe/post?u=514372f571f3cb5abdf8a2637&amp;id=50f2ab4389"
+                      method="post"
+                    >
+                      <input
+                        className="form-control"
+                        type="email"
+                        name="EMAIL"
+                        aria-label="Email Address"
+                        placeholder="Email Address"
+                      />
+                      <div className="input-group-append">
+                        <input
+                          type="submit"
+                          value="Gimme &rarr;"
+                          name="subscribe"
+                          id="mc-embedded-subscribe"
+                          className="btn"
+                        />
+                      </div>
+                    </form>
                   </div>
-                </form>
-              </div>
-            </div>
-            <div className="footer__info">
-              <div className="title">Get Started</div>
-              <a href="/page/about-us">About Us</a>
-              <a href="/blog/">Blog</a>
-              <a href="https://docs.ampled.com/coop/">The Co-Op</a>
-              <a href="/page/contact">Contact</a>
-            </div>
-            <div className="footer__info">
-              <div className="title">Get Involved</div>
-              <a href={config.menuUrls.createArtist}>Create Artist Page</a>
-              <a href="/page/join-the-co-op">Become a Member</a>
-              <a href="/page/become-a-contributor">Jobs</a>
-              <a href="/page/press">Press</a>
-            </div>
-            <div className="footer__info">
-              <div className="title">Get Informed</div>
-              <a href="/page/open-dashboard">Transparency</a>
-              <a href="/page/faq">FAQs</a>
-              <a href="https://docs.ampled.com/policy/terms-of-use">
-                Terms of Use
-              </a>
-              <a href="https://docs.ampled.com/policy/">Policy</a>
-            </div>
-            <div className="footer__logos">
-              <Link to="/">
-                <img src={logo} alt="logo" className="logo" />
-              </Link>
-              <div className="copyright">
-                ©{new Date().getFullYear()} Ampled
-              </div>
-              <div className="own-it">
-                We Own It!{' '}
-                <span role="img" aria-label="Solidarity">
-                  ✊
-                </span>
+                </div>
+
+                <div className="footer__bottom_right">
+                  <div className="own-it">
+                    We Own It! <img src={fist} alt="Solidarity Fist" />
+                  </div>
+                  <div className="footer__bottom_right_links">
+                    <div className="copyright">
+                      ©{new Date().getFullYear()} Ampled
+                    </div>
+                    <a href="https://docs.ampled.com/policy/terms-of-use">
+                      Terms of Use
+                    </a>
+                    <a href="https://docs.ampled.com/policy/">Privacy Policy</a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>

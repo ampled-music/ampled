0dd25b41a178be40c7b685b2e4df243b585c91fc
Ryan Quanz <ryanquanz@gmail.com>
2020-10-19 23:35:39 -0400

    add charge_amount to plan factory

--- spec/factories/plan.rb
+++ spec/factories/plan.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :plan do
     artist_page
     nominal_amount { Random.rand(50_000) }
+    charge_amount { StripeUtil.charge_amount_for_nominal_amount(Money.new(nominal_amount, "usd")).fractional }
     stripe_id { Faker::Alphanumeric.alphanumeric(10) }
   end
 end

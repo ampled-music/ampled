8c138ac83cf34570689101e2231b8b45db65217b
Ryan DeShon <ryandeshon@gmail.com>
2020-10-28 19:23:47 -0400

    Update but name

--- app/jobs/post_notification_email_job.rb
+++ app/jobs/post_notification_email_job.rb
@@ -37,7 +37,7 @@ class PostNotificationEmailJob
           artist_color: artist.accent_color,
           post_title: post.title,
           post_body: post.body,
-          post_type: btn_copy,
+          post_btn_copy: btn_copy,
           post_id: post.id,
           post_url: "#{ENV["REACT_APP_API_URL"]}/artist/#{artist.slug}/post/#{post.id}"
         }

779e48693aed85de301fbd25d49dd3e778586cfc
Corey Farwell <coreyf@rwell.org>
2020-10-31 19:11:31 -0400

    Expand documentation around StripeUtil functions. (#678)
    Reading through the code here for my first time and documented along the way. The hope is that it clarifies what the expected types of the inputs/outputs are as it took me a minute to figure it out. The documentation syntax I'm using here is [YARD].
    
    [YARD]: https://yardoc.org/features.html

--- app/lib/stripe_util.rb
+++ app/lib/stripe_util.rb
@@ -2,6 +2,11 @@ module StripeUtil
   STRIPE_USD_FIXED_FEE = Money.new(30, "USD")
   STRIPE_USD_PERCENTAGE_FEE = 0.029
 
+  # Return a Stripe-compatible three-letter ISO currency string for the given
+  # Money object.
+  #
+  # @param money [Money] Money to determine currency from
+  # @return [String] Stripe-compatible three-letter ISO currency string
   def self.stripe_currency(money)
     money.currency.iso_code.downcase
   end
@@ -9,6 +14,10 @@ module StripeUtil
   # Calculates the amount we need to charge for the nominal_amount
   # to be the after-fee amount (not including Ampled's platform fee).
   # We are charged 2.9% + 30 cents for USD transacitions
+  #
+  # @param nominal_amount [Money] Nominal amount
+  # @return [Money] Calculated charge amount
+  # @raise [StandardError] Raised if the `nominal_amount` currency is not USD
   def self.charge_amount_for_nominal_amount(nominal_amount)
     case stripe_currency(nominal_amount)
     when "usd"

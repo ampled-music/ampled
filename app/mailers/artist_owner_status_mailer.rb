c150bd0260a1790acd46d4a7bebf6629fc6e4bd3
Ryan Quanz <ryanquanz@gmail.com>
2020-11-01 15:39:37 -0500

    add ArtistOwnerStatusMailer for internal email

---
+++ app/mailers/artist_owner_status_mailer.rb
@@ -0,0 +1,12 @@
+class ArtistOwnerStatusMailer < PostmarkMailer
+  # method name must match alias on postmark
+  def internal_artist_eligible_for_ownership(artist_page)
+    self.template_model = {
+      artist_name: artist_page.name,
+      artist_page_url: artist_page.url,
+      page_owners: artist_page.owners.map { |user| { name: user.name, email: user.email } }
+    }
+
+    mail to: ["hello@ampled.com", "austin@ampled.com"]
+  end
+end
---
+++ spec/mailers/artist_owner_status_mailer_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe ArtistOwnerStatusMailer, type: :mailer do
+  describe "#internal_artist_eligible_for_ownership" do
+    let(:owners) do
+      [
+        create(:user, name: "Garfield", email: "garfield@cbs.com"),
+        create(:user, name: "Mr. Bigglesworth", email: "bigglesworth@evil.co")
+      ]
+    end
+    let(:artist_page) { create(:artist_page, name: "Kitteh' Rock", slug: "kittehrock", owners: owners) }
+    let(:mail) { ArtistOwnerStatusMailer.internal_artist_eligible_for_ownership(artist_page) }
+
+    it "sets template" do
+      expect(mail.message.template_model).to eq({
+        artist_name: "Kitteh' Rock",
+        artist_page_url: "http://localhost:3000/artist/kittehrock",
+        page_owners: [
+          {
+            email: "garfield@cbs.com",
+            name: "Garfield"
+          },
+          {
+            email: "bigglesworth@evil.co",
+            name: "Mr. Bigglesworth"
+          }
+        ]
+      })
+    end
+
+    it "sets addresses correctly" do
+      expect(mail.message.to).to eq(["hello@ampled.com", "austin@ampled.com"])
+      expect(mail.message.from).to eq(["dev@ampled.com"])
+    end
+  end
+end

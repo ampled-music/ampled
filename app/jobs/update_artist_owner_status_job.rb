e8a2dc93733c40f1f9914ec944d237cbc5cc0a8e
Ryan Quanz <ryanquanz@gmail.com>
2020-11-01 16:30:09 -0500

    send emails when artist becomes eligible for ownership

--- app/jobs/update_artist_owner_status_job.rb
+++ app/jobs/update_artist_owner_status_job.rb
@@ -5,7 +5,11 @@ class UpdateArtistOwnerStatusJob
   def perform(artist_page_id)
     @artist_page = ArtistPage.find(artist_page_id)
 
-    artist_page.update!(artist_owner: true) if !artist_page.artist_owner? && artist_eligible_to_become_artist_owner?
+    if !artist_page.artist_owner? && artist_eligible_to_become_artist_owner?
+      artist_page.update!(artist_owner: true)
+      ArtistOwnerStatusMailer.internal_artist_eligible_for_ownership(artist_page).deliver_now
+      ArtistOwnerStatusMailer.artist_eligible_for_ownership(artist_page).deliver_now
+    end
   end
 
   private
--- spec/jobs/update_artist_owner_status_job_spec.rb
+++ spec/jobs/update_artist_owner_status_job_spec.rb
@@ -25,11 +25,39 @@ describe UpdateArtistOwnerStatusJob, type: :job do
     context "when an artist page is not an an artist owner but is eligible for artist ownership" do
       before(:each) do
         10.times { create(:subscription, artist_page: artist_page) }
+        allow(ArtistOwnerStatusMailer).to receive(:internal_artist_eligible_for_ownership) do
+          double("Message Delivery", deliver_now: true)
+        end
+        allow(ArtistOwnerStatusMailer).to receive(:artist_eligible_for_ownership) do
+          double("Message Delivery", deliver_now: true)
+        end
       end
 
       it "updates the artist_owner column to true" do
         expect { described_class.new.perform(artist_page.id) }
           .to change { artist_page.reload.artist_owner? }.from(false).to(true)
+      end
+
+      it "sends internal_artist_eligible_for_ownership email" do
+        message_delivery_double = double("Message Delivery")
+        expect(message_delivery_double).to receive(:deliver_now)
+        expect(ArtistOwnerStatusMailer).to(
+          receive(:internal_artist_eligible_for_ownership).with(artist_page)
+            .and_return(message_delivery_double)
+        )
+
+        described_class.new.perform(artist_page.id)
+      end
+
+      it "sends artist_eligible_for_ownership email" do
+        message_delivery_double = double("Message Delivery")
+        expect(message_delivery_double).to receive(:deliver_now)
+        expect(ArtistOwnerStatusMailer).to(
+          receive(:artist_eligible_for_ownership).with(artist_page)
+            .and_return(message_delivery_double)
+        )
+
+        described_class.new.perform(artist_page.id)
       end
     end
   end

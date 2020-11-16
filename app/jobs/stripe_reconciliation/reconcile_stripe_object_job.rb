80ebf3f1c64d350da90d25eae02ee10398d59a1e
Corey Farwell <coreyf@rwell.org>
2020-11-15 22:59:11 -0500

    Add ChargeReconciler; alert if we encounter refunded Stripe Charge.

--- app/jobs/stripe_reconciliation/reconcile_stripe_object_job.rb
+++ app/jobs/stripe_reconciliation/reconcile_stripe_object_job.rb
@@ -6,10 +6,12 @@ module StripeReconciliation
     def perform(stripe_object_hash)
       stripe_object = Stripe::Util.convert_to_stripe_object(stripe_object_hash.symbolize_keys)
       reconciler_class = case stripe_object
+                         when Stripe::Charge
+                           ChargeReconciler
                          when Stripe::Invoice
                            InvoiceReconciler
                          else
-                           log_info("not reconciling Stipe object type #{stripe_object.object}")
+                           log_info("not reconciling Stripe object type #{stripe_object.object}")
                            return
                          end
 

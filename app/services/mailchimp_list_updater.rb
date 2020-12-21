fd7af2649c061920f4a8c76d8b142d6ff545387d
Ryan DeShon <ryandeshon@gmail.com>
2020-12-21 10:26:16 -0500

    Fix most Rubo Cop

--- app/services/mailchimp_list_updater.rb
+++ app/services/mailchimp_list_updater.rb
@@ -1,14 +1,19 @@
 module Services
   class MailchimpListUpdater
-    MailchimpFailed = Class.new(ServiceActionError) def initialize
-      if ENV['MAILCHIMP_API_KEY']
-        @mailchimp = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'], symbolize_keys: true)
-        @mailchimp.timeout = 30
-        @mailchimp.open_timeout = 30
-      end
-    end      def call(user, subscribe = true)
+    MailchimpFailed = Class.new(ServiceActionError)
+
+    def initialize
+      return unless ENV["MAILCHIMP_API_KEY"]
+
+      @mailchimp = Gibbon::Request.new(api_key: ENV["MAILCHIMP_API_KEY"], symbolize_keys: true)
+      @mailchimp.timeout = 30
+      @mailchimp.open_timeout = 30
+    end
+
+    def call(user, subscribe = true)
       raise MailchimpFailed.new unless @mailchimp
-      status = (subscribe ? 'subscribed' : 'unsubscribed')
+
+      status = (subscribe ? "subscribed" : "unsubscribed")
       list(user).upsert(
         body: {
           email_address: user.email,
@@ -22,9 +27,12 @@ module Services
       )
     rescue Gibbon::MailChimpError => e
       raise e
-    end      private      def list(user)
-      @mailchimp.lists(ENV['MAILCHIMP_LIST_ID']).members(
-        user.lower_case_md5_hashed_email)
+    end
+
+    private
+
+    def list(user)
+      @mailchimp.lists(ENV["MAILCHIMP_LIST_ID"]).members(user.lower_case_md5_hashed_email)
     end
   end
 end
--- app/workers/mailchimp/mailchimp_subscriber.rb
+++ app/workers/mailchimp/mailchimp_subscriber.rb
@@ -1,11 +1,17 @@
 module Mailchimp
   class MailchimpSubscriber
-    include Sidekiq::Worker    def perform(user_id, subscribe = true)
+    include Sidekiq::Worker
+
+    def perform(user_id, subscribe = true)
       @user = User.find(user_id)
       mailchimp.call(@user, subscribe)
     rescue StandardError => e
       raise e
-    end    private    def mailchimp
+    end
+
+    private
+
+    def mailchimp
       @mailchimp = Services::MailchimpListUpdater.new
     end
   end

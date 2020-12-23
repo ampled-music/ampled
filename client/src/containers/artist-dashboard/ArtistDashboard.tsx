b8b559c2b25c3f9772956bc450ed88032d9211b2
Ryan DeShon <ryandeshon@gmail.com>
2020-12-23 14:10:29 -0500

    WIP: design

--- client/src/containers/artist-dashboard/ArtistDashboard.tsx
+++ client/src/containers/artist-dashboard/ArtistDashboard.tsx
@@ -1,73 +1,94 @@
 import './artist-dashboard.scss';
 
 import * as React from 'react';
+// import { Link } from 'react-router-dom';
+// import { ReactSVG } from 'react-svg';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import {
+  closeAuthModalAction,
+  openAuthModalAction,
+} from '../../redux/authentication/authentication-modal';
 import { Store } from '../../redux/configure-store';
 import { getMeAction } from '../../redux/me/get-me';
-
-import clsx from 'clsx';
-import {
-  createStyles,
-  lighten,
-  makeStyles,
-  Theme,
-} from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-
+import { setUserDataAction } from '../../redux/me/set-me';
+import { showToastAction } from '../../redux/toast/toast-modal';
 import { Image, Transformation } from 'cloudinary-react';
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableSortLabel,
+} from '@material-ui/core';
+// import { apiAxios } from '../../api/setup-axios';
+
+// import { faImage } from '@fortawesome/free-solid-svg-icons';
+// import { faStripe } from '@fortawesome/free-brands-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import tear from '../../images/backgrounds/background_tear.png';
+// import tear_black from '../../images/backgrounds/background_tear_black.png';
+
+// import Edit from '../../images/icons/Icon_Edit.svg';
+// import Instagram from '../../images/icons/Icon_Instagram.svg';
+// import Twitter from '../../images/icons/Icon_Twitter.svg';
+// import Location from '../../images/icons/Icon_Location.svg';
+
+import { initialState as loginInitialState } from '../../redux/authentication/initial-state';
+import { initialState as meInitialState } from '../../redux/me/initial-state';
+import { initialState as subscriptionsInitialState } from '../../redux/subscriptions/initial-state';
+// import { routePaths } from '../route-paths';
+// import { Modal } from '../shared/modal/Modal';
+// import { ResetPassword } from '../connect/ResetPassword';
+// import { Loading } from '../shared/loading/Loading';
+// import { UserImage } from '../user-details/UserImage';
 
 const mapDispatchToProps = (dispatch) => ({
   getMe: bindActionCreators(getMeAction, dispatch),
+  setMe: bindActionCreators(setUserDataAction, dispatch),
+  openAuthModal: bindActionCreators(openAuthModalAction, dispatch),
+  closeAuthModal: bindActionCreators(closeAuthModalAction, dispatch),
+  showToast: bindActionCreators(showToastAction, dispatch),
 });
 
+type Dispatchers = ReturnType<typeof mapDispatchToProps>;
+
+type Props = typeof loginInitialState &
+  typeof meInitialState &
+  Dispatchers & {
+    history: any;
+    location: any;
+    subscriptions: typeof subscriptionsInitialState;
+  };
+
 interface Data {
-  calories: number;
-  carbs: number;
-  fat: number;
   name: string;
-  protein: number;
+  monthly: number;
+  all_time: number;
+  location: string;
+  supporting_since: string;
 }
 
 function createData(
   name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
+  monthly: number,
+  all_time: number,
+  location: string,
+  supporting_since: string,
 ): Data {
-  return { name, calories, fat, carbs, protein };
+  return { name, monthly, all_time, location, supporting_since };
 }
 
 const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
+  createData('Test Name 1', 6.0, 159, 'Place', '2020-10-29 19:51:43.717274'),
+  createData('Test Name 2', 9.0, 237, 'Place', '2020-10-29 19:51:43.717274'),
+  createData('Test Name 3', 3.0, 262, 'Place', '2020-10-29 19:51:43.717274'),
+  createData('Test Name 4', 3.7, 305, 'Place', '2020-10-29 19:51:43.717274'),
+  createData('Test Name 5', 4.0, 356, 'Place', '2020-10-29 19:51:43.717274'),
 ];
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
@@ -112,20 +133,29 @@ interface HeadCell {
 }
 
 const headCells: HeadCell[] = [
+  { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
   {
-    id: 'name',
-    numeric: false,
-    disablePadding: true,
-    label: 'Dessert (100g serving)',
+    id: 'monthly',
+    numeric: true,
+    disablePadding: false,
+    label: 'Monthy Support',
   },
-  { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-  { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-  { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+  {
+    id: 'all_time',
+    numeric: true,
+    disablePadding: false,
+    label: 'All Time Support',
+  },
+  { id: 'location', numeric: false, disablePadding: false, label: 'Location' },
+  {
+    id: 'supporting_since',
+    numeric: false,
+    disablePadding: false,
+    label: 'Supporting Since',
+  },
 ];
 
 interface EnhancedTableProps {
-  classes: ReturnType<typeof useStyles>;
   numSelected: number;
   onRequestSort: (
     event: React.MouseEvent<unknown>,
@@ -139,7 +169,6 @@ interface EnhancedTableProps {
 
 function EnhancedTableHead(props: EnhancedTableProps) {
   const {
-    classes,
     onSelectAllClick,
     order,
     orderBy,
@@ -156,14 +185,6 @@ function EnhancedTableHead(props: EnhancedTableProps) {
   return (
     <TableHead>
       <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{ 'aria-label': 'select all desserts' }}
-          />
-        </TableCell>
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
@@ -178,7 +199,7 @@ function EnhancedTableHead(props: EnhancedTableProps) {
             >
               {headCell.label}
               {orderBy === headCell.id ? (
-                <span className={classes.visuallyHidden}>
+                <span>
                   {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
                 </span>
               ) : null}
@@ -190,101 +211,7 @@ function EnhancedTableHead(props: EnhancedTableProps) {
   );
 }
 
-const useToolbarStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(1),
-    },
-    highlight:
-      theme.palette.type === 'light'
-        ? {
-            color: theme.palette.secondary.main,
-            backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-          }
-        : {
-            color: theme.palette.text.primary,
-            backgroundColor: theme.palette.secondary.dark,
-          },
-    title: {
-      flex: '1 1 100%',
-    },
-  }),
-);
-
-interface EnhancedTableToolbarProps {
-  numSelected: number;
-}
-
-const EnhancedTableToolbar = (props: EnhancedTableToolbarProps) => {
-  const classes = useToolbarStyles();
-  const { numSelected } = props;
-
-  return (
-    <Toolbar
-      className={clsx(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
-      {numSelected > 0 ? (
-        <Typography
-          className={classes.title}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography
-          className={classes.title}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Nutrition
-        </Typography>
-      )}
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton aria-label="delete"></IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton aria-label="filter list"></IconButton>
-        </Tooltip>
-      )}
-    </Toolbar>
-  );
-};
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '100%',
-    },
-    paper: {
-      width: '100%',
-      marginBottom: theme.spacing(2),
-    },
-    table: {
-      minWidth: 750,
-    },
-    visuallyHidden: {
-      border: 0,
-      clip: 'rect(0 0 0 0)',
-      height: 1,
-      margin: -1,
-      overflow: 'hidden',
-      padding: 0,
-      position: 'absolute',
-      top: 20,
-      width: 1,
-    },
-  }),
-);
-
-class ArtistDashboardComponent extends React.Component<any, any> {
+class ArtistDashboardComponent extends React.Component<Props, any> {
   state = {};
 
   componentDidMount() {
@@ -298,7 +225,7 @@ class ArtistDashboardComponent extends React.Component<any, any> {
     console.log(userData);
 
     return (
-      <>
+      <div className="artist-dashboard__panel">
         <Image
           publicId={userData.image.public_id}
           alt={userData.name}
@@ -317,159 +244,48 @@ class ArtistDashboardComponent extends React.Component<any, any> {
         <h2>
           {userData.name} {userData.last_name}
         </h2>
-      </>
+      </div>
     );
   }
 
   renderSupportersPanel() {
-    const classes = useStyles();
-    const [order, setOrder] = React.useState<Order>('asc');
-    const [orderBy, setOrderBy] = React.useState<keyof Data>('calories');
-    const [selected, setSelected] = React.useState<string[]>([]);
-    const [page, setPage] = React.useState(0);
-    const [dense, setDense] = React.useState(false);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    return (
+      <TableContainer className="artist-dashboard__data">
+        <Table stickyHeader aria-label="Supporters table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Monthly</TableCell>
+              <TableCell>All Time</TableCell>
+              <TableCell>Location</TableCell>
+              <TableCell>Supporting Since</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell>{row.monthly}</TableCell>
+                <TableCell>{row.all_time}</TableCell>
+                <TableCell>{row.location}</TableCell>
+                <TableCell>{row.supporting_since}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
 
-    const handleRequestSort = (
-      event: React.MouseEvent<unknown>,
-      property: keyof Data,
-    ) => {
-      const isAsc = orderBy === property && order === 'asc';
-      setOrder(isAsc ? 'desc' : 'asc');
-      setOrderBy(property);
-    };
-
-    const handleSelectAllClick = (
-      event: React.ChangeEvent<HTMLInputElement>,
-    ) => {
-      if (event.target.checked) {
-        const newSelecteds = rows.map((n) => n.name);
-        setSelected(newSelecteds);
-        return;
-      }
-      setSelected([]);
-    };
-
-    const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
-      const selectedIndex = selected.indexOf(name);
-      let newSelected: string[] = [];
-
-      if (selectedIndex === -1) {
-        newSelected = newSelected.concat(selected, name);
-      } else if (selectedIndex === 0) {
-        newSelected = newSelected.concat(selected.slice(1));
-      } else if (selectedIndex === selected.length - 1) {
-        newSelected = newSelected.concat(selected.slice(0, -1));
-      } else if (selectedIndex > 0) {
-        newSelected = newSelected.concat(
-          selected.slice(0, selectedIndex),
-          selected.slice(selectedIndex + 1),
-        );
-      }
-
-      setSelected(newSelected);
-    };
-
-    const handleChangePage = (event: unknown, newPage: number) => {
-      setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (
-      event: React.ChangeEvent<HTMLInputElement>,
-    ) => {
-      setRowsPerPage(parseInt(event.target.value, 10));
-      setPage(0);
-    };
-
-    const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setDense(event.target.checked);
-    };
-
-    const isSelected = (name: string) => selected.indexOf(name) !== -1;
-
-    const emptyRows =
-      rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+  render() {
+    const { userData } = this.props;
 
     return (
-      <div className={classes.root}>
-        <Paper className={classes.paper}>
-          <EnhancedTableToolbar numSelected={selected.length} />
-          <TableContainer>
-            <Table
-              className={classes.table}
-              aria-labelledby="tableTitle"
-              size={dense ? 'small' : 'medium'}
-              aria-label="enhanced table"
-            >
-              <EnhancedTableHead
-                classes={classes}
-                numSelected={selected.length}
-                order={order}
-                orderBy={orderBy}
-                onSelectAllClick={handleSelectAllClick}
-                onRequestSort={handleRequestSort}
-                rowCount={rows.length}
-              />
-              <TableBody>
-                {stableSort(rows, getComparator(order, orderBy))
-                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map((row, index) => {
-                    const isItemSelected = isSelected(row.name);
-                    const labelId = `enhanced-table-checkbox-${index}`;
-
-                    return (
-                      <TableRow
-                        hover
-                        onClick={(event) => handleClick(event, row.name)}
-                        role="checkbox"
-                        aria-checked={isItemSelected}
-                        tabIndex={-1}
-                        key={row.name}
-                        selected={isItemSelected}
-                      >
-                        <TableCell padding="checkbox">
-                          <Checkbox
-                            checked={isItemSelected}
-                            inputProps={{ 'aria-labelledby': labelId }}
-                          />
-                        </TableCell>
-                        <TableCell
-                          component="th"
-                          id={labelId}
-                          scope="row"
-                          padding="none"
-                        >
-                          {row.name}
-                        </TableCell>
-                        <TableCell align="right">{row.calories}</TableCell>
-                        <TableCell align="right">{row.fat}</TableCell>
-                        <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
-                      </TableRow>
-                    );
-                  })}
-                {emptyRows > 0 && (
-                  <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
-                    <TableCell colSpan={6} />
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[5, 10, 25]}
-            component="div"
-            count={rows.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onChangePage={handleChangePage}
-            onChangeRowsPerPage={handleChangeRowsPerPage}
-          />
-        </Paper>
-        <FormControlLabel
-          control={<Switch checked={dense} onChange={handleChangeDense} />}
-          label="Dense padding"
-        />
+      <div className="artist-dashboard">
+        {userData && this.renderArtistPanel()}
+        {this.renderSupportersPanel()}
       </div>
     );
   }

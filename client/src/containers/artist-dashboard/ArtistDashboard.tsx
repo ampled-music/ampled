4034d7f25b354ac06dc580862d15fa3807771bda
Ryan DeShon <ryandeshon@gmail.com>
2020-12-23 16:36:54 -0500

    Use DataGrid

--- client/src/containers/artist-dashboard/ArtistDashboard.tsx
+++ client/src/containers/artist-dashboard/ArtistDashboard.tsx
@@ -13,20 +13,14 @@ import { Store } from '../../redux/configure-store';
 import { getMeAction } from '../../redux/me/get-me';
 import { setUserDataAction } from '../../redux/me/set-me';
 import { showToastAction } from '../../redux/toast/toast-modal';
+
 import { Image, Transformation } from 'cloudinary-react';
-import {
-  Button,
-  IconButton,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow,
-  TableSortLabel,
-} from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, IconButton } from '@material-ui/core';
 // import { apiAxios } from '../../api/setup-axios';
+
+import { DataGrid, RowsProp, ColDef } from '@material-ui/data-grid';
 
 import Plus from '../../images/icons/Icon_Add-New.svg';
 import Settings from '../../images/icons/Icon_Settings.svg';
@@ -50,6 +44,12 @@ import { initialState as subscriptionsInitialState } from '../../redux/subscript
 // import { Loading } from '../shared/loading/Loading';
 // import { UserImage } from '../user-details/UserImage';
 
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
 const mapDispatchToProps = (dispatch) => ({
   getMe: bindActionCreators(getMeAction, dispatch),
   setMe: bindActionCreators(setUserDataAction, dispatch),
@@ -68,151 +68,96 @@ type Props = typeof loginInitialState &
     subscriptions: typeof subscriptionsInitialState;
   };
 
-interface Data {
-  name: string;
-  monthly: number;
-  all_time: number;
-  location: string;
-  supporting_since: string;
-}
-
-function createData(
-  name: string,
-  monthly: number,
-  all_time: number,
-  location: string,
-  supporting_since: string,
-): Data {
-  return { name, monthly, all_time, location, supporting_since };
-}
-
-const rows = [
-  createData('Test Name 1', 6.0, 159, 'Place', '2020-10-29 19:51:43.717274'),
-  createData('Test Name 2', 9.0, 237, 'Place', '2020-10-29 19:51:43.717274'),
-  createData('Test Name 3', 3.0, 262, 'Place', '2020-10-29 19:51:43.717274'),
-  createData('Test Name 4', 3.7, 305, 'Place', '2020-10-29 19:51:43.717274'),
-  createData('Test Name 5', 4.0, 356, 'Place', '2020-10-29 19:51:43.717274'),
-];
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-type Order = 'asc' | 'desc';
-
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key,
-): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string },
-) => number {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-interface HeadCell {
-  disablePadding: boolean;
-  id: keyof Data;
-  label: string;
-  numeric: boolean;
-}
-
-const headCells: HeadCell[] = [
-  { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
+const rows: RowsProp = [
   {
-    id: 'monthly',
-    numeric: true,
-    disablePadding: false,
-    label: 'Monthy Support',
+    id: 1,
+    name: 'Test Name 1',
+    monthly: 0,
+    all_time: 0,
+    location: 'Location',
+    supporting_since: '2020-10-29 19:51:43.717274',
   },
   {
-    id: 'all_time',
-    numeric: true,
-    disablePadding: false,
-    label: 'All Time Support',
+    id: 2,
+    name: 'Test Name 2',
+    monthly: 0,
+    all_time: 0,
+    location: 'Location',
+    supporting_since: '2020-10-29 19:51:43.717274',
   },
-  { id: 'location', numeric: false, disablePadding: false, label: 'Location' },
   {
-    id: 'supporting_since',
-    numeric: false,
-    disablePadding: false,
-    label: 'Supporting Since',
+    id: 3,
+    name: 'Test Name 3',
+    monthly: 0,
+    all_time: 0,
+    location: 'Location',
+    supporting_since: '2020-10-29 19:51:43.717274',
+  },
+  {
+    id: 4,
+    name: 'Test Name 4',
+    monthly: 0,
+    all_time: 0,
+    location: 'Location',
+    supporting_since: '2020-10-29 19:51:43.717274',
+  },
+  {
+    id: 5,
+    name: 'Test Name 5',
+    monthly: 0,
+    all_time: 0,
+    location: 'Location',
+    supporting_since: '2020-10-29 19:51:43.717274',
+  },
+  {
+    id: 6,
+    name: 'Test Name 6',
+    monthly: 0,
+    all_time: 0,
+    location: 'Location',
+    supporting_since: '2020-10-29 19:51:43.717274',
+  },
+  {
+    id: 7,
+    name: 'Test Name 7',
+    monthly: 0,
+    all_time: 0,
+    location: 'Location',
+    supporting_since: '2020-10-29 19:51:43.717274',
+  },
+  {
+    id: 8,
+    name: 'Test Name 8',
+    monthly: 0,
+    all_time: 0,
+    location: 'Location',
+    supporting_since: '2020-10-29 19:51:43.717274',
+  },
+  {
+    id: 9,
+    name: 'Test Name 9',
+    monthly: 0,
+    all_time: 0,
+    location: 'Location',
+    supporting_since: '2020-10-29 19:51:43.717274',
+  },
+  {
+    id: 10,
+    name: 'Test Name 10',
+    monthly: 0,
+    all_time: 0,
+    location: 'Location',
+    supporting_since: '2020-10-29 19:51:43.717274',
   },
 ];
 
-interface EnhancedTableProps {
-  numSelected: number;
-  onRequestSort: (
-    event: React.MouseEvent<unknown>,
-    property: keyof Data,
-  ) => void;
-  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  order: Order;
-  orderBy: string;
-  rowCount: number;
-}
-
-function EnhancedTableHead(props: EnhancedTableProps) {
-  const {
-    onSelectAllClick,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort,
-  } = props;
-  const createSortHandler = (property: keyof Data) => (
-    event: React.MouseEvent<unknown>,
-  ) => {
-    onRequestSort(event, property);
-  };
-
-  return (
-    <TableHead>
-      <TableRow>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
-            padding={headCell.disablePadding ? 'none' : 'default'}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <span>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </span>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
+const columns: ColDef[] = [
+  { field: 'name', headerName: 'Name', width: 400 },
+  { field: 'monthly', headerName: 'Monthly Support', width: 150 },
+  { field: 'all_time', headerName: 'All Time Support', width: 150 },
+  { field: 'location', headerName: 'Location', width: 400 },
+  { field: 'supporting_since', headerName: 'Supporting Since', width: 200 },
+];
 
 class ArtistDashboardComponent extends React.Component<Props, any> {
   state = {};
@@ -265,33 +210,11 @@ class ArtistDashboardComponent extends React.Component<Props, any> {
   }
 
   renderSupportersPanel() {
+    // const classes = useStyles();
     return (
-      <TableContainer className="artist-dashboard__data">
-        <Table stickyHeader aria-label="Supporters table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Monthly</TableCell>
-              <TableCell>All Time</TableCell>
-              <TableCell>Location</TableCell>
-              <TableCell>Supporting Since</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell>{row.monthly}</TableCell>
-                <TableCell>{row.all_time}</TableCell>
-                <TableCell>{row.location}</TableCell>
-                <TableCell>{row.supporting_since}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <div className="artist-dashboard__data">
+        <DataGrid rows={rows} columns={columns} />
+      </div>
     );
   }
 

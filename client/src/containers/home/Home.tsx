ecebf134fdc69eaecbaf1dccbe58111b20f97961
Ryan DeShon <ryandeshon@gmail.com>
2021-01-18 18:51:12 -0500

    Clean up

--- client/src/containers/home/Home.tsx
+++ client/src/containers/home/Home.tsx
@@ -1,9 +1,6 @@
 import './home.scss';
 
 import * as React from 'react';
-import cx from 'classnames';
-import * as store from 'store';
-import { ReactSVG } from 'react-svg';
 
 import { HomeArtists } from './HomeArtists';
 import { HomeFor } from './HomeFor';
@@ -13,10 +10,6 @@ import { HomeGarden } from './HomeGarden';
 import { Texture } from '../shared/texture/Texture';
 
 class Home extends React.Component<any> {
-  state = {
-    showBanner: store.get('close-banner') ? false : true,
-  };
-
   render() {
     return (
       <div className="home-section">

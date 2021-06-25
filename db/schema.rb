475b85949b6037f38f66c17e2b2a59a3c1e8a9b3
ryanquanz <ryanquanz@gmail.com>
2021-06-25 13:14:46 -0400

    set application_fee_percent to 7 for everyone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_19_015523) do
+ActiveRecord::Schema.define(version: 2021_06_25_171139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

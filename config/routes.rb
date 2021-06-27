6a2a142e6899161a92089d5d48739b39f1bbfda9
Sean Adams <sean@seanadams.org>
2021-06-27 10:29:15 -0400

    add backend cloudinary route

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Rails.application.routes.draw do
 
   get "uploads/sign", to: "uploads#sign_file"
   get "uploads/playable_url", to: "uploads#playable_url"
+  post "uploads/cloudinary", to: "uploads#cloudinary_upload"
 
   resources :me, only: [:index] do
     put :update_card, on: :collection

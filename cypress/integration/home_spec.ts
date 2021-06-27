12809064f0a3b04d167ed8f38f444ba61dac4816
Sean Adams <sean@seanadams.org>
2021-06-27 13:23:26 -0400

    log artist page response

--- cypress/integration/home_spec.ts
+++ cypress/integration/home_spec.ts
@@ -5,7 +5,9 @@ describe('Ampled Home Page', () => {
   })
 
   it('loads', () => {
+    cy.intercept('GET', '/artist_pages.json').as('artist_pages')
     cy.visit('/')
+    cy.wait('@artist_pages').then(xhr => cy.log(JSON.stringify(xhr.response.body)))
     cy.contains('Ampled')
     cy.get('.home-artists').parent().scrollIntoView()
     cy.get('.home-artists__title').contains('5 artists')

022adfec0f82cb57fab8a74fa673a79ec42154fd
Ryan DeShon <ryandeshon@gmail.com>
2021-07-21 09:58:01 -0400

    Clean up and link

--- client/src/containers/dashboard/CardRoadMap.tsx
+++ client/src/containers/dashboard/CardRoadMap.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import { Card, CardContent } from '@material-ui/core';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrello } from '@fortawesome/free-brands-svg-icons';
 import ArtistHandbook from '../../images/icons/Icon_Artist-Handbook.png';
 
 export const CardRoadMap = () => {
@@ -11,20 +9,17 @@ export const CardRoadMap = () => {
       <CardContent>
         <div className="dashboard__home_card_title">Learn More</div>
         <div className="row dashboard__home_card_roadmap_content">
-          <div className="col-6">
-            <img src={ArtistHandbook} alt="Artist Handbook" />
+          <div className="col-12 col-sm-6">
+            <a href="https://docs.ampled.com/artist-handbook/">
+              <img src={ArtistHandbook} alt="Artist Handbook" />
+            </a>
           </div>
-          <div className="col-6">
+          <div className="col-12 col-sm-6">
             <div className="dashboard__home_card_roadmap_info">
               <h5>Roadmap</h5>
               <p>See what Ampled contributors are working on.</p>
               <div className="dashboard__home_card_by">
                 Powered by Notion
-                {/* <FontAwesomeIcon
-                  className="dashboard__home_card_by_icon"
-                  icon={faTrello}
-                  size="2x"
-                /> */}
               </div>
             </div>
           </div>

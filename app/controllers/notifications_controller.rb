81fc409e7fcc5d446728184966a308dc16a83b4c
Sean Adams <sean@seanadams.org>
2021-09-18 14:07:32 -0400

    remove policy

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,24 +1,19 @@
 class NotificationsController < ApplicationController
   before_action :set_notification, only: %i[destroy mark_as_read]
+  before_action :check_ownership, only: %i[destroy mark_as_read]
 
   def index
     @notifications = current_user&.unread_notifications
   end
 
   def destroy
-    if policy.modify? && @notification.destroy
-      200
-    else
-      render_not_allowed
-    end
+    @notification.destroy
+    head(:ok)
   end
 
   def mark_as_read
-    if policy.modify? && @notification.read!
-      200
-    else
-      render_not_allowed
-    end
+    @notification.read!
+    head(:ok)
   end
 
   private
@@ -33,5 +28,14 @@ class NotificationsController < ApplicationController
 
   def set_notification
     @notification = Notification.find(params[:id])
+  end
+
+  def check_ownership
+    return if can_modify_notification?
+    render_not_allowed
+  end
+
+  def can_modify_notification?
+    current_user&.admin? || current_user&.id == @notification.user_id
   end
 end

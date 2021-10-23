c5bae84750debeda61a28d11d73b70eb648dc4f8
Ryan DeShon <ryandeshon@gmail.com>
2021-10-23 12:21:56 -0400

    Add Meetings and Events

--- client/src/containers/dashboard/CardRoadMap.tsx
+++ client/src/containers/dashboard/CardRoadMap.tsx
@@ -9,25 +9,37 @@ export const CardRoadMap = () => {
       <CardContent>
         <div className="dashboard__home_card_title">Learn More</div>
         <div className="row dashboard__home_card_roadmap_content">
-          <div className="col-12 col-sm-6">
+        <div className="col-12 col-sm-4">
+            <div className="dashboard__home_card_roadmap_info">
+              <h5>
+                <a 
+                  href="https://ampled.notion.site/6670468bb8d94bdabc4f29b5fe518ac8" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >Meetings & Events</a>
+              </h5>
+              <p>Stay up to date with the co-op</p>
+            </div>
+          </div>
+          <div className="col-12 col-sm-4">
             <a href="https://docs.ampled.com/artist-handbook/">
               <img src={ArtistHandbook} alt="Artist Handbook" />
             </a>
           </div>
-          <div className="col-12 col-sm-6">
-              <div className="dashboard__home_card_roadmap_info">
-                <h5>
-                  <a 
-                    href="https://ampled.notion.site/424ae126d5ec40fb8f9beb3c65c52f34" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                  >Roadmap</a>
-                </h5>
-                <p>See what Ampled contributors are working on.</p>
-                <div className="dashboard__home_card_by">
-                  Powered by Notion
-                </div>
+          <div className="col-12 col-sm-4">
+            <div className="dashboard__home_card_roadmap_info">
+              <h5>
+                <a 
+                  href="https://ampled.notion.site/424ae126d5ec40fb8f9beb3c65c52f34" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >Roadmap</a>
+              </h5>
+              <p>See what Ampled contributors are working on.</p>
+              <div className="dashboard__home_card_by">
+                Powered by Notion
               </div>
+            </div>
           </div>
         </div>
       </CardContent>

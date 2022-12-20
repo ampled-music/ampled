afad2b78133f2d6cf2bfaf09faf526ca75fb80c7
Ryan Deshon <ryandeshon@gmail.com>
2022-12-20 17:07:41 -0500

    Init Payment Action Required

---
+++ app/jobs/card_action_required_email_job.rb
@@ -0,0 +1,20 @@
+class CardActionRequiredEmailJob
+  include Sidekiq::Worker
+  attr_accessor :subscription
+
+  def perform(subscription_id)
+    @subscription = Subscription.find(subscription_id)
+    return if subscription.blank?
+
+    SendBatchEmail.call(
+      [{
+        from: ENV["POSTMARK_FROM_EMAIL"],
+        to: subscription.user.email,
+        template_alias: "credit_card_action_required",
+        template_model: {
+          artist: subscription.artist_page.name
+        }
+      }]
+    )
+  end
+end
